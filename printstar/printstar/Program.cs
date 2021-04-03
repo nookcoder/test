45dfbb029ffbdead66e11ba65bd2fba16fa18122
HYEONWOOK5319 <gus70005319@users.noreply.github.com>
2021-04-04 02:01:38 +0900

    가운데 정렬 별 찍기 클래스 생성

---
+++ printstar/printstar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace printstar
+{
+    class Program
+    {
+        class Bigger
+        {
+            public int Line; 
+            public void PrintingBigger()
+            {
+                
+                for(int i = 1; i < Line + 1; i++)
+                {
+                    for(int k = Line; k > i; k-- )
+                    {
+                        Console.Write(" "); 
+                    }
+                    for(int j = 0; j < (2*i - 1); j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+        class MainApp
+        {
+
+            static void Main(string[] args)
+            {
+             
+            }
+        }
+
+        
+        
+    }
+}
