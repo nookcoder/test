a7684769873a80177ed16b72bfb6c4b9c9478be8
HYEONWOOK5319 <gus70005319@users.noreply.github.com>
2021-04-04 16:36:52 +0900

    반복 기능 추가

--- printstar/printstar/Program.cs
+++ printstar/printstar/Program.cs
@@ -82,40 +82,48 @@ namespace printstar
 
             static void Main(string[] args)
             {
-                int type;
-                int line;
-                Console.WriteLine("1.가운데 정렬 별 찍기\n");
-                Console.WriteLine("2.1번 반대로 찍기\n");
-                Console.WriteLine("3.모래 시계 찍기\n");
-                Console.WriteLine("4.다이아 찍기");
-                type = Convert.ToInt32(Console.ReadLine()); // 무슨 모양으로 출력할 것 인지 결정 
-                
-                Console.WriteLine("몇 줄 만들고 싶으세요? : ");
-                line = Convert.ToInt32(Console.ReadLine()); // 몇 줄 만들건지 결정 
-
+                int type; // 어떤 모양으로 출력할 지 결정할 변수
+                int line; // 몇 줄을 출력할 지 결정할 변수
+                int again = 1; // 끝낼 건지 아닌 지 결정할 변수 
                 Bigger bigger = new Bigger();
                 Smaller smaller = new Smaller();
-                Diamond diamond = new Diamond(); 
-                
-                switch(type)
-                {
-                    case 1: // 가운데 정렬 찍기일 때 
-                        bigger.PrintingBigger(line);
-                        break;
+                Diamond diamond = new Diamond();
 
-                    case 2: // 1번 반대로 찍기 일 때 
-                        smaller.PrintingSmaller(line);
-                        break;
 
-                    case 3: // 모래시계 찍기
-                        smaller.PrintingSmaller(line);
-                        bigger.PrintingBigger(line);
-                        break;
-                    case 4: // 다이아몬드 찍기
-                        diamond.PrintingDiamond(line);
-                        break; 
-                        
-                }
+                while (again != '2') {
+                    Console.WriteLine("1.가운데 정렬 별 찍기\n");
+                    Console.WriteLine("2.1번 반대로 찍기\n");
+                    Console.WriteLine("3.모래 시계 찍기\n");
+                    Console.WriteLine("4.다이아 찍기");
+                    type = Convert.ToInt32(Console.ReadLine()); // 무슨 모양으로 출력할 것 인지 결정 
+
+                    Console.WriteLine("몇 줄 만들고 싶으세요? : ");
+                    line = Convert.ToInt32(Console.ReadLine()); // 몇 줄 만들건지 결정 
+
+                    switch (type)
+                    {
+                        case 1: // 가운데 정렬 찍기일 때 
+                            bigger.PrintingBigger(line);
+                            break;
+
+                        case 2: // 1번 반대로 찍기 일 때 
+                            smaller.PrintingSmaller(line);
+                            break;
+
+                        case 3: // 모래시계 찍기
+                            smaller.PrintingSmaller(line);
+                            bigger.PrintingBigger(line);
+                            break;
+                        case 4: // 다이아몬드 찍기
+                            diamond.PrintingDiamond(line);
+                            break;
+
+                    }
+                    Console.WriteLine("다시시작 : 1 종료 : 2 \n");
+                    again = int.Parse(Console.ReadLine()); 
+              
+
+                } 
                 
 
 
