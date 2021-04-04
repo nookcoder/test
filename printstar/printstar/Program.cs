69d33fc3521e00465aae78f14925f6d10d3b8518
HYEONWOOK5319 <gus70005319@users.noreply.github.com>
2021-04-04 15:47:50 +0900

    1번, 2번 찍기 class 내에 매개 변수 전달 및 클래스 변수 삭제 / 모양 선택 과 출력( 다이아몬드 찍기 제외)

--- printstar/printstar/Program.cs
+++ printstar/printstar/Program.cs
@@ -6,13 +6,12 @@ namespace printstar
     {
         class Bigger // 가운데 별 찍기 
         {
-            public int Line; // 입력 받은 줄 수
-            public void PrintingBigger() // 별 찍기 함수 
+            public void PrintingBigger(int line) // 별 찍기 함수 
             {
                 
-                for(int i = 1; i < Line + 1; i++)
+                for(int i = 1; i < line + 1; i++)
                 {
-                    for(int k = Line; k > i; k-- )
+                    for(int k = line; k > i; k-- )
                     {
                         Console.Write(" "); 
                     }
@@ -27,17 +26,16 @@ namespace printstar
 
         class Smaller // 가운데 별 찍기 반대로 찍기
         {
-            public int Line; // 줄 갯수 
             
-            public void PrintingSmaller() // 반대로 찍기 함수 
+            public void PrintingSmaller(int line) // 반대로 찍기 함수 
             {
-                for (int i = 1; i < Line + 1; i++)
+                for (int i = 1; i < line + 1; i++)
                 {
                     for (int k = 1; k < i; k++)
                     {
                         Console.Write(" ");
                     }
-                    for (int j = 2 * Line; j > (2 * i - 1); j--)
+                    for (int j = 2 * line; j > (2 * i - 1); j--)
                     {
                         Console.Write("*");
                     }
@@ -45,15 +43,47 @@ namespace printstar
                 }
             }
         }
-        class MainApp
+        class PrintStar
         {
 
             static void Main(string[] args)
             {
-        
-
-        
+                int type;
+                int line;
+                Console.WriteLine("1.가운데 정렬 별 찍기\n");
+                Console.WriteLine("2.1번 반대로 찍기\n");
+                Console.WriteLine("3.모래 시계 찍기\n");
+                Console.WriteLine("4.다이아 찍기");
+                type = Convert.ToInt32(Console.ReadLine()); // 무슨 모양으로 출력할 것 인지 결정 
                 
+                Console.WriteLine("몇 줄 만들고 싶으세요? : ");
+                line = Convert.ToInt32(Console.ReadLine()); // 몇 줄 만들건지 결정 
+
+                Bigger bigger = new Bigger();
+                Smaller smaller = new Smaller();
+                
+                switch(type)
+                {
+                    case 1: // 가운데 정렬 찍기일 때 
+                        bigger.PrintingBigger(line);
+                        break;
+
+                    case 2: // 1번 반대로 찍기 일 때 
+                        smaller.PrintingSmaller(line);
+                        break;
+
+                    case 3: // 모래시계 찍기
+                        smaller.PrintingSmaller(line);
+                        bigger.PrintingBigger(line);
+                        break;
+                    case 4:
+                        bigger.PrintingBigger(line-1);
+                        smaller.PrintingSmaller(line);
+                        break;
+                }
+                
+
+
             }
         }
 
