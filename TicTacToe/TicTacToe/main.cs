41cb35f25fa42c7bd80483a2eab0d48a91b41258
HYEONWOOK5319 <gus70005319@users.noreply.github.com>
2021-04-07 02:19:36 +0900

    Tic Tac Toe 재설계 // 게임 설명 기능 추가

---
+++ TicTacToe/TicTacToe/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        
+        static void Main(string[] args)
+        {
+            Start start = new Start();
+            start.Introduction();
+            start.StartGame();
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/start.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+ public class Start
+ {
+    private int start = 1;
+    private int end = 2; 
+    public void Introduction()
+    {
+        Console.WriteLine("          -----------------             ");
+        Console.WriteLine("          |  TIc Tac Toe  |             ");
+        Console.WriteLine("          -----------------             ");
+        Console.WriteLine("\n");
+        Console.WriteLine(" ------------ 게임 설명 ----------------");
+        Console.WriteLine("\n");
+        Console.WriteLine(" 1. 3 X 3 9개의 칸 안에 두 사람이 번갈아");
+        Console.WriteLine("    가면서 돌을 두는 3목 게임");
+        Console.WriteLine(" 2. 연달아 3개의 O나 X를 먼저 그리는 사람이");
+        Console.WriteLine("    이긴다. ");
+        Console.WriteLine("\n");
+        Console.WriteLine(" -------------------------------------- ");
+        Console.WriteLine(" 다음으로 넘어가려면 아무키나 눌러주세요.");
+        Console.ReadKey();
+    }
+
+    public void StartGame()
+    {
+        Console.WriteLine("으악!");
+    }
+
+ }
+
+      
