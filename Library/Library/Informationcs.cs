07b39a5928e51549cdc59bc2d327aa189f831c88
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-11 04:47:55 +0900

    [ADD]  (refactory 예정)  정보 입력 기능 추가

---
+++ Library/Library/Informationcs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Get
+    {
+        public string id = string.Empty;
+        public string password = string.Empty;
+
+        public void GetInformation()
+        {
+            Console.Write("아이디 : ");
+            id = Console.ReadLine();
+            Console.Write("비밀번호 : ");
+            password = Console.ReadLine(); 
+        }
+    }
+}
---
+++ Library/Library/Member.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Member
+    {
+        public Member(string _id, string _password)
+        {
+            this.id = _id;
+            this.password = _password; 
+        }
+
+        private string id;
+        private string password;
+        public string Id
+        {
+            get { return id; }
+            set { id = value;  }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        
+       
+
+
+    }
+}
