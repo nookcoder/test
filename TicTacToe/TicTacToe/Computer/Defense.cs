c32089468a09197318c7ebed725459f551e4631b
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-10 22:13:17 +0900

    [ADD] Computer 2칸이 채워졌을 때 공격 방어 기능 추가

---
+++ TicTacToe/TicTacToe/Computer/Defense.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Defense
+    {
+        private char[] array;
+        public int i = 0;
+        public int j = 0;
+        public int point;
+
+        public Defense(char[] _array)
+        {
+            for (i = 0; i < 9; i++)
+            {
+                this.array[i] = _array[i];
+            }
+        }
+
+        public int Row()
+        {
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 3 * i; j < 3 * i + 3; j++)
+                {
+                    if ((array[j] == 'X' && array[j + 1] == 'X') ||
+                       (array[j] == 'X' && array[j + 2] == 'X') ||
+                       (array[j + 1] == 'X' && array[j + 2] == 'X'))
+                    {
+                        if (array[j] != 'O')
+                        {
+                            point = j;
+                        }
+
+                        else if (array[j + 1] != 'O')
+                        {
+                            point = j + 1;
+                        }
+
+                        else
+                        {
+                            point = j + 2;
+                        }
+
+                    }
+                }
+            }
+
+            return point;
+        }
+
+        public int Col()
+        {
+            for (i = 0; i < 3; i++)
+            {
+                if ((array[i] == 'X' && array[i + 3] == 'X') ||
+                       (array[i] == 'X' && array[i + 6] == 'X') ||
+                       (array[i + 3] == 'X' && array[i + 6] == 'X'))
+                {
+                    if (array[i] != 'O')
+                    {
+                        point = i;
+                    }
+
+                    else if (array[i + 3] != 'O')
+                    {
+                        point = i + 3;
+                    }
+
+                    else
+                    {
+                        point = i + 6;
+                    }
+
+                }
+            }
+
+            return point;
+        }
+
+        public int TopLeftToBottonRight()
+        {
+            if ((array[0] == 'X' && array[4] == 'X') ||
+                       (array[4] == 'X' && array[8] == 'X') ||
+                       (array[0] == 'X' && array[9] == 'X'))
+            {
+                if (array[0] != 'O')
+                {
+                    point = 1;
+                }
+
+                else if (array[4] != 'O')
+                {
+                    point = 5;
+                }
+
+                else
+                {
+                    point = 9;
+                }
+            }
+
+            return point;
+        }
+        public int TopRightToBottonLeft()
+        {
+            if ((array[2] == 'X' && array[4] == 'X') ||
+                       (array[4] == 'X' && array[6] == 'X') ||
+                       (array[0] == 'X' && array[6] == 'X'))
+            {
+                if (array[2] != 'O')
+                {
+                    point = 3;
+                }
+
+                else if (array[4] != 'O')
+                {
+                    point = 5;
+                }
+
+                else
+                {
+                    point = 7;
+                }
+            }
+
+            return point;
+        }
+    }
+
+
+
+}
+
---
+++ TicTacToe/TicTacToe/Computer/Win.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Win
+    {
+        char[] array;
+        public int i = 0;
+        public int j = 0;
+        public int point;
+        public Win(char[] _array)
+        {
+            for (i = 0; i < 9; i++)
+            {
+                this.array[i] = _array[i];
+            }
+        }
+
+        public int Row()
+        {
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 3 * i; j < 3 * i + 3; j++)
+                {
+                    if ((array[j] == 'O' && array[j + 1] == 'O') ||
+                       (array[j] == 'O' && array[j + 2] == 'O') ||
+                       (array[j + 1] == 'O' && array[j + 2] == 'O'))
+                    {
+                        if (array[j] != 'O')
+                        {
+                            point = j;
+                        }
+
+                        else if (array[j + 1] != 'O')
+                        {
+                            point = j + 1;
+                        }
+
+                        else
+                        {
+                            point = j + 2;
+                        }
+
+                    }
+                }
+            }
+
+            return point;
+        }
+
+        public int Col()
+        {
+            for (i = 0; i < 3; i++)
+            {
+                if ((array[i] == 'O' && array[i + 3] == 'O') ||
+                       (array[i] == 'O' && array[i + 6] == 'O') ||
+                       (array[i + 3] == 'O' && array[i + 6] == 'O'))
+                {
+                    if (array[i] != 'O')
+                    {
+                        point = i;
+                    }
+
+                    else if (array[i + 3] != 'O')
+                    {
+                        point = i + 3;
+                    }
+
+                    else
+                    {
+                        point = i + 6;
+                    }
+
+                }
+            }
+
+            return point;
+        }
+
+        public int TopLeftToBottonRight()
+        {
+            if ((array[0] == 'O' && array[4] == 'O') ||
+                       (array[4] == 'O' && array[8] == 'O') ||
+                       (array[0] == 'O' && array[9] == 'O'))
+            {
+                if (array[0] != 'O')
+                {
+                    point = 1;
+                }
+
+                else if (array[4] != 'O')
+                {
+                    point = 5;
+                }
+
+                else
+                {
+                    point = 9;
+                }
+            }
+
+            return point;
+        }
+        public int TopRightToBottonLeft()
+        {
+            if ((array[2] == 'O' && array[4] == 'O') ||
+                       (array[4] == 'O' && array[6] == 'O') ||
+                       (array[0] == 'O' && array[6] == 'O'))
+            {
+                if (array[2] != 'O')
+                {
+                    point = 3;
+                }
+
+                else if (array[4] != 'O')
+                {
+                    point = 5;
+                }
+
+                else
+                {
+                    point = 7;
+                }
+            }
+
+            return point;
+        }
+    }
+}
