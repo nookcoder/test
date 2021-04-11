4f602456dafd83d52ee8219dbdf85efd61982229
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-11 23:27:32 +0900

    [REFACTORY] 책 정보 class 개편

--- Library/Library/Book.cs
+++ Library/Library/Book.cs
@@ -5,76 +5,80 @@ using System.Text;
 
 namespace Library
 {
-    public class Book 
+    public class Book
     {
-        private string number; 
-        private string title;
-        private string publisher;
-        private string author; 
-        private bool isBookOk; // 책을 빌릴 수 있는 지 아닌 지 여부
-        ArrayList booklist = new ArrayList(); 
+        // 도서 정보 클래스. 
 
-        public Book()
+        public Book(int _number, string _title, string _publicher, string _author)
         {
-        
+            this.Number = _number;
+            this.Title = _title;
+            this.Publisher = _publicher;
+            this.Author = _author; 
         }
 
-        public string Number 
+        public int Number
         {
-            get { return number; }
-            set { number = value; }
+            get;
+            set;
         }
 
         public string Title
         {
-            get { return title; }
-            set { title = value; }
+            get;
+            set;
         }
 
         public string Publisher
         {
-            get { return publisher; }
-            set { publisher = value; }
+            get;
+            set;
         }
 
         public string Author
         {
-            get { return number; }
-            set { number = value; }
+            get;
+            set;
         }
 
-        public bool IsBookOk
+        public override string ToString()
         {
-            get { return isBookOk; }
-            set { isBookOk = value;   }
-
-        }
-        public string getBookNumber()
-        {
-            return number;
+            return $"제목 : {Title}, 저자 : {Author}";
         }
 
-        public string getBookTitle()
-        {
-            return title; 
-        }
 
-        public string getPublisher()
-        {
-            return publisher;
-        }
-
-        public string getAuthor()
-        {
-            return author; 
-        }
-
-        public bool getIsBookOk()
-        {
-            return isBookOk; 
-        }      
-        
     }
 
+    public class BookManagement
+    {
+        public BookManagement()
+        {
+
+        }
+        public void InsertBook()
+        {
+            int number;
+            string title;
+            string publisher;
+            string author; 
+            ArrayList bookList = new ArrayList();
+
+            Console.Write("도서 번호 : ");
+            number = int.Parse(Console.ReadLine()); 
+            Console.Write("책 제목 : ");
+            title = Console.ReadLine();
+            Console.Write("출판사 :  ");
+            publisher = Console.ReadLine();
+            Console.Write("작가 이름 : ");
+            author = Console.ReadLine();
+
+            Book book = new Book(number, title, publisher, author);
+            bookList.Add(book);
+
+            Console.WriteLine($"{bookList[0]}"); 
+        }
+    }
 
 }
+
+
