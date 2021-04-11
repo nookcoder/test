fc81c6e8313dd4a78fa35cb0bd369a7131516b86
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-11 16:58:23 +0900

    [ADD] 중복 되는 Console.WriteLine 함수로 처리 기능

---
+++ TicTacToe/TicTacToe/Paint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Paint
+    {
+        public void PaintUnderLine()
+        {
+            Console.WriteLine("  -------------------------");
+        }
+
+        public void PaintVerticalLine()
+        {
+            Console.WriteLine("  |       |       |       |");
+        }
+    }
+}
