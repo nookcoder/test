1733d35c7033dce59df1f0ac21da4f22605cb0c2
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-12 16:43:03 +0900

    [ADD] 등록되있는 전체 도서 출력기능 추가

--- Library/Library/BookManagement.cs
+++ Library/Library/BookManagement.cs
@@ -47,7 +47,7 @@ namespace Library
                     break;
 
                 case print:
-                    //SelectAll();
+                    SelectAll();
                     break;
 
                 case rental:
@@ -64,129 +64,9 @@ namespace Library
             }
         }
 
-        public void SelectOne()
-        {
-            string selectMenu;
-            bool isDone = false;
 
-            while (!isDone)
-            {
-                BookPage bookPage = new BookPage();
-                PrintGetTitle();
-                selectMenu = PrintGetMoreSearch();
-                Console.Clear();
-
-                if (selectMenu == "1")
-                {
-                    bookPage.PrintBookMenu();
-                    PrintGetTitle();
-                    selectMenu = PrintGetMoreSearch();
-                    Console.Clear();
-                }
-
-                else if (selectMenu == "2")
-                {
-                    PrintBookMain();
-                    isdone = true;
-                    break;
-                }
-
-                else
-                {
-                    ReviseErrorSelectOne();
-                    Console.Clear();
-                    bookPage.PrintBookMenu();
-                }
-            }
-
-        }
-
-        public string PrintGetMoreSearch()
-        {
-            string selectMenu;
-            Console.WriteLine("다른 책을 검색하시겠습니까 ?");
-            Console.WriteLine("[1] 다시 검색");
-            Console.WriteLine("[2] 종료 하기");
-            selectMenu = Console.ReadLine();
-            
-            return selectMenu;
-        }
-
-        public void PrintGetTitle()
-        {
-            string bookTitle;
-
-            Console.Write("찾으시는 책 제목을 입력해주세요 : ");
-            bookTitle = Console.ReadLine();
-
-            if (bookList.Count != 0)
-            {
-                for (int bookListIndex = 0; bookListIndex < bookList.Count; bookListIndex++)
-                {
-                    if (bookList[bookListIndex].Title == bookTitle)
-                    {
-                        Console.WriteLine(bookList[bookListIndex]);
-                        if (bookList[bookListIndex].IsOk)
-                        {
-                            Console.WriteLine("대여여부 : 가능");
-                        }
-                    }
-
-                    else
-                    {
-                        Console.WriteLine("죄송하지만 해당 책은 존재하지 않습니다. ");
-                        Console.WriteLine(" ");
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("죄송하지만 해당 책은 존재하지 않습니다. ");
-                Console.WriteLine(" ");
-            }
-        }
-
-        public void GetBookInfo()
-        {
-            Console.Write("도서 번호 : ");
-            number = int.Parse(Console.ReadLine());
-            Console.Write("책 제목 : ");
-            title = Console.ReadLine();
-            Console.Write("출판사 :  ");
-            publisher = Console.ReadLine();
-            Console.Write("작가 이름 : ");
-            author = Console.ReadLine();
-            bookList.Add(new Book(number, title, publisher, author, true));
-            Console.WriteLine($"등록이 완료되었습니다.");
-            Console.WriteLine(" ");
-        }
-
-        public void ReviseErrorSelectOne()
-        {
-            string selectMenu;
-            bool isError = true;
-            BookPage bookPage = new BookPage();
-
-            while (isError)
-            {
-                Console.Clear();
-                bookPage.PrintBookMenu();
-                Console.WriteLine("");
-                Console.WriteLine("잘못 입력하셨습니다.");
-                Console.WriteLine("");
-
-                selectMenu = PrintGetMoreSearch();
-
-                if (selectMenu == "1" || selectMenu == "2")
-                {
-                    isError = false;
-                }
-
-            }
-        }
-
-
-
+        // [1] 도서 추가 하기 
+        // 도서 추가 하기 호출 함수. 
         public void InsertBook()
         {
             string addition;
@@ -212,7 +92,177 @@ namespace Library
                 }
             }
         }
+
+        // 책의 정보를 물어보는 함수. 
+        public void GetBookInfo()
+        {
+            Console.Write("도서 번호 : ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write("책 제목 : ");
+            title = Console.ReadLine();
+            Console.Write("출판사 :  ");
+            publisher = Console.ReadLine();
+            Console.Write("작가 이름 : ");
+            author = Console.ReadLine();
+            bookList.Add(new Book(number, title, publisher, author, true));
+            Console.WriteLine($"등록이 완료되었습니다.");
+            Console.WriteLine(" ");
+        }
+
+
+        // [3]원하는 도서를 찾는 함수.
+        // 원하는 도서 출력하기 호출 함수.
+        public void SelectOne()
+        {
+            string selectMenu;
+            bool isDone = false;
+            BookPage bookPage = new BookPage();
+
+            while (!isDone)
+            {
+                PrintGetTitle();
+                selectMenu = PrintGetMoreSearch();
+                Console.Clear();
+
+                if (selectMenu == "1")
+                {
+                    bookPage.PrintBookMenu();
+                    PrintGetTitle();
+                    selectMenu = PrintGetMoreSearch();
+                    Console.Clear();
+                }
+
+                else if (selectMenu == "2")
+                {
+                    PrintBookMain();
+                    isdone = true;
+                }
+
+                else
+                {
+                    ReviseErrorSelectOne();
+                    Console.Clear();
+                    bookPage.PrintBookMenu();
+                }
+            }
+
+        }
+
+        // 다시 검색하고 싶은 지 묻는 함수.
+        public string PrintGetMoreSearch()
+        {
+            string selectMenu;
+            Console.WriteLine("다른 책을 검색하시겠습니까 ?");
+            Console.WriteLine("[1] 다시 검색");
+            Console.WriteLine("[2] 종료 하기");
+            selectMenu = Console.ReadLine();
+
+            return selectMenu;
+        }
+
+        // 제목을 입력받아 해당 책이 있으면 출력하는 함수. 
+        public void PrintGetTitle()
+        {
+            string bookTitle;
+
+            Console.Write("찾으시는 책 제목을 입력해주세요 : ");
+            bookTitle = Console.ReadLine();
+
+            if (bookList.Count != 0)
+            {
+                for (int bookListIndex = 0; bookListIndex < bookList.Count; bookListIndex++)
+                {
+                    if (bookList[bookListIndex].Title == bookTitle)
+                    {
+                        Console.WriteLine(bookList[bookListIndex]);
+                        if (bookList[bookListIndex].IsOk)
+                        {
+                            Console.WriteLine("대여여부 : 가능");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("대여여부 : 불가능");
+
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("죄송하지만 해당 책은 존재하지 않습니다. ");
+                        Console.WriteLine(" ");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("죄송하지만 해당 책은 존재하지 않습니다. ");
+                Console.WriteLine(" ");
+            }
+        }
+
+        // 다시 검색 질문에서 잘못된 값을 입력 했을 때 처리 함수. 
+        public void ReviseErrorSelectOne()
+        {
+            string selectMenu;
+            bool isError = true;
+            BookPage bookPage = new BookPage();
+
+            while (isError)
+            {
+                Console.Clear();
+                bookPage.PrintBookMenu();
+                Console.WriteLine("");
+                Console.WriteLine("잘못 입력하셨습니다.");
+                Console.WriteLine("");
+
+                selectMenu = PrintGetMoreSearch();
+
+                if (selectMenu == "1" || selectMenu == "2")
+                {
+                    isError = false;
+                }
+
+            }
+        }
+
+        // [4] 전체 도서 출력하기 
+        // 전체 도서 출력하기 호출 함수
+        public void SelectAll()
+        {
+            BookPage bookPage = new BookPage();
+            if (bookList.Count != 0)
+            {
+                for (int bookListIndex = 0; bookListIndex < bookList.Count; bookListIndex++)
+                {
+                    bookPage.PirntBar();
+                    Console.WriteLine($"{bookList[bookListIndex]}");
+                    bookPage.PirntBar();
+                }
+            }
+
+            else
+            {
+                Console.Clear();
+                bookPage.PrintBookMenu();
+                Console.WriteLine(" "); 
+                Console.WriteLine("등록된 도서가 없습니다. "); 
+            }
+
+            GoBack();
+        }
+
+        public void GoBack()
+        {
+            Console.WriteLine(" "); 
+            Console.Write("뒤로가려면 아무키나 눌러주세요.... ");
+            Console.ReadKey();
+            Console.Clear();
+            PrintBookMain();
+        }
     }
+
+
 
     public class BookPage
     {
@@ -230,6 +280,11 @@ namespace Library
             Console.Write("[5] : 도서 대여\n");
             Console.Write("[6] : 도서 반납\n");
             Console.Write("[7] : 뒤로 가기\n");
+        }
+
+        public void PirntBar()
+        {
+            Console.WriteLine("--------------------");
         }
 
         public int GetBookMenuNumber()
