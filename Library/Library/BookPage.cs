1bb72603658fa1e13658d4bc3ec8232e4abc9e88
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-12 22:12:58 +0900

    [SET] BookPage class 다른 파일로 분리

---
+++ Library/Library/BookPage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class BookPage
+    {
+        public BookPage()
+        {
+
+        }
+
+        public void PrintBookMenu()
+        {
+            Console.Write("[1] : 도서 등록\n");
+            Console.Write("[2] : 도서 수정\n");
+            Console.Write("[3] : 도서 검색\n");
+            Console.Write("[4] : 도서 출력\n");
+            Console.Write("[5] : 도서 대여\n");
+            Console.Write("[6] : 도서 반납\n");
+            Console.Write("[7] : 뒤로 가기\n");
+        }
+
+        public void PirntBar()
+        {
+            Console.WriteLine(" -------------------- ");
+        }
+
+        public int GetBookMenuNumber()
+        {
+            int bookMenu;
+            bookMenu = int.Parse(Console.ReadLine());
+            Console.Clear();
+            PrintBookMenu();
+
+            return bookMenu;
+        }
+
+        // 에러 출력 함수 
+        public void PrintError()
+        {
+
+            BookPage bookPage = new BookPage();
+
+            Console.Clear();
+            bookPage.PrintBookMenu();
+            PirntBar();
+            Console.WriteLine("| 잘못 입력하셨습니다.| ");
+            PirntBar();
+
+        }
+    }
+}
+}
