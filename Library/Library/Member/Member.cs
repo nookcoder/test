f356cf687965755e00663f4a2aab9565c7d04881
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-13 09:33:50 +0900

    [ADD] 유저 정보 클래스 추가

---
+++ Library/Library/Member/Member.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    public class Member
+    {
+        public Member(string _userName, string _phoneNumber,string _id, string _password)
+        {
+            this.UserName = _userName;
+            this.PhoneNumber = _phoneNumber;
+            this.Id = _id;
+            this.Password = _password;
+        }
+
+        public string UserName
+        {
+            get;
+            set;
+        }
+
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+
+        public string Id
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set; 
+        }
+        
+       
+
+
+    }
+}
