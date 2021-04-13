9b452de9a0e9803c1ee035fbc42a76c23463ceb3
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-13 09:31:47 +0900

    [ADD] 라이브러리 첫 실행 출력 화면 추가

--- Library/Library/Main.cs
+++ Library/Library/Main.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Library
 {
@@ -8,11 +6,61 @@ namespace Library
     {
         public Main()
         {
-            BookMain bookMain = new BookMain();
-            bookMain.ShowBookMain();
+            Console.SetWindowSize(45, 30);
+            SelectFirstMenu();
+        }
+
+        // Main 클래스 생성할 때 호출될 함수. 
+        public void SelectFirstMenu()
+        {
+            string firstMenu;
+            PrintFirstPage(); 
+            Console.Write("                메뉴 입력:");
+            firstMenu = Console.ReadLine();
+            HandleExceptFirstMenu(firstMenu);
+
+        }
+
+        // 도서관 실행 시 나타날 화면 출력
+        public void PrintFirstPage()
+        {
+            Console.WriteLine("    ==================================");
+            Console.WriteLine("");
+            Console.WriteLine("             [1] 회원 로그인");
+            Console.WriteLine("");
+            Console.WriteLine("             [2] 관리자 로그인    ");
+            Console.WriteLine("");
+            Console.WriteLine("    ==================================");
+        }
+
+        // 메뉴 입력 예외 처리
+        public void HandleExceptFirstMenu(string _firstMenu)
+        {
+            bool isError = true;
+            while (isError)
+            {
+                if (_firstMenu == "1")
+                {
+                    isError = false;
+                    // MemberLogint memberLogin = new MemberLogin();
+                }
+
+                else if (_firstMenu == "2")
+                {
+                    isError = false; 
+                    // ManagerLogin managerLogin = new ManagerLogin(); 
+                }
+
+                else
+                {
+                    Console.Clear();
+                    PrintFirstPage();
+                    Console.WriteLine("           잘못 입력하셨습니다.");
+                    Console.WriteLine("    ==================================");
+                    Console.Write("                메뉴 입력:");
+                    _firstMenu = Console.ReadLine();
+                }
+            }
         }
     }
-
-
-
 }
