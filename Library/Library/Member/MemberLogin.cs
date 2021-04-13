8f4df2ff9c08a084c6f3a98cb68481fd81fff2ee
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-13 09:32:48 +0900

    [ADD] 유저 로그인 화면 추가

---
+++ Library/Library/Member/MemberLogin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class MemberLogin
+    {
+        public MemberLogin()
+        {
+            Console.SetWindowSize(50, 30);
+        }
+
+        List<Member> members = new List<Member>();
+        
+        
+        public void PrintUserMenu()
+        {
+            Console.WriteLine("    ==================================");
+            Console.WriteLine(" ");
+            Console.WriteLine("              [1] 회원 가입           ");
+            Console.WriteLine("              [2]  로그인             ");
+            Console.WriteLine("              [2] 회원 탈퇴           ");
+            Console.WriteLine("");
+            Console.WriteLine("    ==================================");
+        }
+
+    }
+}
