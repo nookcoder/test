2fe7c42658e09b718da1bbde8fe4c0afa9517093
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:47:28 +0900

    [ADD] 첫번째 페이지에서 다음페이지로 넘어가는 기능 추가

---
+++ ReLibrary/ReLibrary/Controller/FirstPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.View;
+using ReLibrary.Model;
+
+namespace ReLibrary.Controller
+{
+    class FirstPage
+    {
+        public FirstPage()
+        {
+            Console.SetWindowSize(60, 30);
+            Screen screen = new Screen();
+            screen.PrintLabel();
+            screen.PrintFirstPage();
+            screen.PrintInput();
+            GoSecondPage();
+        }
+
+        public void GoSecondPage()
+        {
+            Except except = new Except();
+            FirstPageModel firstPageModel = new FirstPageModel();
+            Input input = new Input();
+
+            string check;
+            int menu;
+            
+            check = input.RequestInput();
+            menu = except.HandleFirstMenuExcept(check);
+            firstPageModel.GuideFirstPageMenu(menu);
+        }
+    }
+
+}
