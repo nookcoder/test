34b57467b0e9edf3bdde4d3c927a61ab2e927fec
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 00:39:28 +0900

    [ADD] 첫번째 페이지 예외처리 기능 추가

---
+++ ReLibrary/ReLibrary/Model/Except.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.View;
+
+namespace ReLibrary.Model
+{
+    class Except
+    {
+        public int HandleFirstMenuExcept(string check)
+        {
+            int menu = Constants.RESET;
+            bool isError = true;
+
+            while(isError)
+            {
+                if(check == "1" || check == "2" || check == "3")
+                {
+                    menu = int.Parse(check);
+                    isError =Constants.NOERROR;
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Common common = new Common();
+                    FirstPage firstPage = new FirstPage();
+                    common.PrintPageError();
+                    common.PrintInput();
+                    check = Console.ReadLine();
+                }
+            }
+
+            return menu; 
+        }
+    }
+}
