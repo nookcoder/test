4d50a291dc1cfa5c865cdede688d354245550949
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:47:54 +0900

    [ADD] 입력받는 함수 생성

---
+++ ReLibrary/ReLibrary/Controller/Input.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLibrary.Controller
+{
+    class Input
+    {
+        public Input()
+        {
+        }
+        public string RequestInput()
+        {
+            string menu;
+            menu = Console.ReadLine();
+
+            return menu;
+        }
+    }
+}
