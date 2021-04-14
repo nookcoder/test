f690f09f57f354b1cf757e9c958db71754da2a58
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 00:40:33 +0900

    [ADD]  메뉴 입력받는 기능 추가

---
+++ ReLibrary/ReLibrary/Controller/FirstPageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.View;
+using ReLibrary.Model;
+
+namespace ReLibrary.Controller
+{
+    class FirstPageController
+    {
+        public FirstPageController()
+        {
+            Console.SetWindowSize(60, 30);
+            Common common = new Common();
+            FirstPage firstPage = new FirstPage();
+            common.PrintInput();
+            GoSecondPage();
+        }
+        public string GETFirstPage()
+        {
+            string menu;
+            menu = Console.ReadLine();
+
+            return menu;
+        }
+
+        public void GoSecondPage()
+        {
+            Except except = new Except();
+            FirstPageModel firstPageModel = new FirstPageModel();
+            string check;
+            int menu;
+            
+            check = GETFirstPage();
+            menu = except.HandleFirstMenuExcept(check);
+            firstPageModel.TakeFirstPageMenu(menu);
+        }
+    }
+
+}
