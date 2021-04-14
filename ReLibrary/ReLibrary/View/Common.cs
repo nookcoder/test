07ef386a31bb7475d4085b2b1ca7cab6d69f536f
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 00:38:27 +0900

    [ADD] 제목, 경고 문구, 메뉴 입력 출력 함수 추가

---
+++ ReLibrary/ReLibrary/View/Common.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLibrary.View
+{
+    class Common
+    {
+        public Common()
+        {
+            PrintLabel();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void PrintLabel()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("");
+            Console.WriteLine("============================================================");
+            Console.WriteLine(" ㅁ       ㅁ ㅁㅁㅁㅁ ㅁㅁㅁㅁ    ㅁㅁ    ㅁㅁㅁㅁ ㅁ    ㅁ ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁ    ㅁ ㅁ    ㅁ   ㅁ  ㅁ   ㅁ    ㅁ   ㅁ ㅁ  ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁㅁㅁㅁ ㅁㅁㅁ    ㅁㅁㅁㅁ  ㅁㅁㅁ       ㅁ   ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁ    ㅁ ㅁ    ㅁ ㅁ      ㅁ ㅁ    ㅁ     ㅁ   ");
+            Console.WriteLine(" ㅁㅁㅁㅁ ㅁ ㅁㅁㅁㅁ ㅁ    ㅁ ㅁ      ㅁ ㅁ     ㅁ    ㅁ   ");
+            Console.WriteLine("============================================================");
+        }
+
+        public void PrintInput()
+        {
+            Console.Write("                       입력 : ");
+        }
+
+        public void PrintPageError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    잘못 입력했습니다                       ");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
