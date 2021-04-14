14eeab58d2662b22fd6d85a0eabf62491035719e
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:28:58 +0900

    [ADD] UserLoginMenu 예외처리 기능 추가

--- ReLibrary/ReLibrary/Model/Except.cs
+++ ReLibrary/ReLibrary/Model/Except.cs
@@ -26,7 +26,7 @@ namespace ReLibrary.Model
                 {
                     Console.Clear();
                     Common common = new Common();
-                    FirstPage firstPage = new FirstPage();
+                    FirstPageScreen firstPage = new FirstPageScreen();
                     common.PrintPageError();
                     common.PrintInput();
                     check = Console.ReadLine();
@@ -34,6 +34,33 @@ namespace ReLibrary.Model
             }
 
             return menu; 
+        }
+
+        public int HandleUserloginExcept(string check)
+        {
+            int menu = Constants.RESET;
+            bool isError = true;
+
+            while (isError)
+            {
+                if (check == "1" || check == "2" || check == "3" || check == "4"|| check == "5")
+                {
+                    menu = int.Parse(check);
+                    isError = Constants.NOERROR;
+                }
+
+                else
+                {
+                    Console.Clear();
+                    Common common = new Common();
+                    UserLoginScreen userLoginScreen = new UserLoginScreen();
+                    common.PrintPageError();
+                    common.PrintInput();
+                    check = Console.ReadLine();
+                }
+            }
+
+            return menu;
         }
     }
 }
