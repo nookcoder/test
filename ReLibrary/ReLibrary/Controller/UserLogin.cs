7700ea201bf9d66dc8ef96d19de4634780086242
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:49:49 +0900

    [ADD] UserLogin 페이지 생성

---
+++ ReLibrary/ReLibrary/Controller/UserLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.Model;
+using ReLibrary.View;
+
+namespace ReLibrary.Controller
+{
+    class UserLogin
+    {
+        public UserLogin()
+        {
+            Screen screen = new Screen();
+            Console.Clear();
+            screen.PrintLabel();
+            screen.PrintUserLoginPage();
+            screen.PrintInput();
+            GoUserLoginNextMenu();
+        }
+
+        public void GoUserLoginNextMenu()
+        {
+            Except except = new Except();
+            UserLoginModel userLoginModel = new UserLoginModel();
+            string check;
+            int menu;
+
+            check = Console.ReadLine();
+            menu = except.HandleUserloginExcept(check);
+            userLoginModel.GuideUserLoginMenu(menu);
+        }
+    }
+}
