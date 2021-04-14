595ba88b5dd462e43f5249d5d93923eac21cec03
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:28:24 +0900

    [RENAME] 변수명에  _ 추가

--- ReLibrary/ReLibrary/Model/Constants.cs
+++ ReLibrary/ReLibrary/Model/Constants.cs
@@ -10,31 +10,31 @@ namespace ReLibrary.Model
     public class Constants
     {
         // 초기 화면 
-        public const int USERMENU = 1;
-        public const int MANAGERMENU = 2;
+        public const int USER_MENU = 1;
+        public const int MANAGER_MENU = 2;
         public const int QUIT = 3;
 
         // 회원 메뉴 
-        public const int USERLOGIN = 1;
-        public const int USERSIGNUP = 2;
-        public const int USER1WITHDRAW = 3;
-        public const int USERBASCK = 4;
-        public const int USERBACK = 5;
+        public const int USER_LOGIN = 1;
+        public const int USER_SIGNUP = 2;
+        public const int USER_WITHDRAW = 3;
+        public const int USER_FIND = 4;
+        public const int USER_BACK = 5;
 
         // 회원 도서 메뉴 
-        public const int SEARCHBOOK = 1;
-        public const int BORROWBOOK = 2;
-        public const int RETURNBOOK = 3;
-        public const int BOOKBACK = 4; 
+        public const int SEARCH_BOOK = 1;
+        public const int BORROW_BOOK = 2;
+        public const int RETURN_BOOK = 3;
+        public const int BOOK_BACk = 4; 
 
         // 관리자 메뉴 
-        public const int USERINFORMATION = 1;
-        public const int MANAGERWITHDARW = 2;
-        public const int RESISTERBOOK = 3;
-        public const int REVISEBOOK = 4;
-        public const int DELETBOOK = 5;
-        public const int PRINTBOOK = 6;
-        public const int MANAGERBACK = 7;
+        public const int USER_INFORMATION = 1;
+        public const int MANAGER_WITHDARW = 2;
+        public const int RESISTER_BOOK = 3;
+        public const int REVISE_BOOK = 4;
+        public const int DELET_BOOK = 5;
+        public const int PRINT_BOOK = 6;
+        public const int MANAGER_BACK = 7;
 
         // 기타
         public const bool NOERROR = false;
