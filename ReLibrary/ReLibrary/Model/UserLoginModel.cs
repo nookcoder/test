a815d2097bcb48dce7cc262dba239d54d2c6ab64
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 01:50:38 +0900

    [ADD] 다음 화면으로 넘어갈 수 있는 함수 생성

---
+++ ReLibrary/ReLibrary/Model/UserLoginModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.Controller;
+
+namespace ReLibrary.Model
+{
+    class UserLoginModel
+    {
+        public void GuideUserLoginMenu(int menu)
+        {
+            switch (menu)
+            {
+                case Constants.USER_LOGIN:
+                    break;
+
+                case Constants.USER_SIGNUP:
+                    break;
+
+                case Constants.USER_FIND:
+                    break;
+
+                case Constants.USER_WITHDRAW:
+                    break;
+
+                case Constants.USER_BACK:
+                    Console.Clear();
+                    FirstPage first = new FirstPage();
+                    break;
+            }
+        }
+    }
+}
