5bad4479551c37abda6bccb80f3df52c78fb1ffd
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-14 22:53:11 +0900

    [ADD] 첫번째 화면 UI 추가

---
+++ ReLibrary/ReLibrary/View/FirstPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLibrary.View
+{
+    public class FirstPage
+    {
+        public FirstPage()
+        {
+            Label label = new Label();
+            PrintFirstPage();
+        }
+
+        public void PrintFirstPage()
+        {
+            Console.WriteLine("============================================================");
+            Console.WriteLine("                    [1] 회원 입니다                         ");
+            Console.WriteLine("                    [2] 관리자 입니다                       ");
+            Console.WriteLine("============================================================");
+
+        }
+    }
+}
---
+++ ReLibrary/ReLibrary/View/Label.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLibrary.View
+{
+    class Label
+    {
+        public Label()
+        {
+            PrintLabel();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void PrintLabel()
+        {
+            Console.SetWindowSize(60, 30);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("");
+            Console.WriteLine("============================================================");
+            Console.WriteLine(" ㅁ       ㅁ ㅁㅁㅁㅁ ㅁㅁㅁㅁ    ㅁㅁ    ㅁㅁㅁㅁ ㅁ    ㅁ ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁ    ㅁ ㅁ    ㅁ   ㅁ  ㅁ   ㅁ    ㅁ   ㅁ ㅁ  ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁㅁㅁㅁ ㅁㅁㅁ    ㅁㅁㅁㅁ  ㅁㅁㅁ       ㅁ   ");
+            Console.WriteLine(" ㅁ       ㅁ ㅁ    ㅁ ㅁ    ㅁ ㅁ      ㅁ ㅁ    ㅁ     ㅁ   ");
+            Console.WriteLine(" ㅁㅁㅁㅁ ㅁ ㅁㅁㅁㅁ ㅁ    ㅁ ㅁ      ㅁ ㅁ     ㅁ    ㅁ   ");
+            Console.WriteLine("============================================================");
+        }
+    }
+}
