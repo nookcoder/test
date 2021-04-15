79e77b6a27e20d7851cf8bafe8e2fe692978551e
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 23:37:47 +0900

    [ADD] 회원가입 관련 화면 추가

--- ReLibrary/ReLibrary/Screen.cs
+++ ReLibrary/ReLibrary/Screen.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReLibrary.View
+namespace ReLibrary
 {
     class Screen
     {
@@ -43,6 +43,14 @@ namespace ReLibrary.View
             Console.WriteLine("============================================================");
         }
 
+        public void PrintUser()
+        {
+            Console.Clear();
+            PrintLabel();
+            PrintUserLoginPage();
+            PrintInput();
+        }
+
         public void PrintInput()
         {
             Console.Write("                       입력 : ");
@@ -54,5 +62,67 @@ namespace ReLibrary.View
             Console.WriteLine("                    잘못 입력했습니다                       ");
             Console.ForegroundColor = ConsoleColor.White;
         }
+
+        // 회원가입 화면에 쓸 함수
+        public void PrintSignUpName(string name)
+        {
+            Console.Clear();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.Write("                    이름     : {0}", name);
+        }
+
+        public void PrintSignUpPhoneNumber(string phoneNumber)
+        {
+            Console.WriteLine("\n");
+            Console.Write("                    전화번호(-제외) : {0}", phoneNumber);
+        }
+
+        public void PrintSignUpAddress(string address)
+        {
+            Console.WriteLine("\n");
+            Console.Write("                    주소 : {0}", address);
+        }
+
+        public void PrintSignUpId(string id)
+        {
+            Console.WriteLine("\n");
+            Console.Write("                    아이디 : {0}", id);
+        }
+
+        public void PrintSignUpId()
+        {
+            Console.WriteLine("\n");
+            Console.Write("                    비밀번호 : {0}");
+        }
+
+        public void PrintNameError()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" ");
+            Console.WriteLine("                    올바르지 않은 형식입니다.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                    이름     : ");
+        }
+
+        public void PrintPhoneNumberError(string name)
+        {
+            Console.Clear();
+            PrintSignUpName(name);
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("                    올바르지 않은 형식입니다.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("                    전화번호(-제외) : ");
+
+        }
+
+        public void PrintIdError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("8자이상 입력해주세요");
+            Console.ForegroundColor = ConsoleColor.White;
+        } 
     }
 }
