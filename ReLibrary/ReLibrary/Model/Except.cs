f8c992c08bde5abf701ffd28ef91581c32743b82
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-15 23:38:51 +0900

    [ADD] 이름, 전화번호 예외처리 추가

--- ReLibrary/ReLibrary/Model/Except.cs
+++ ReLibrary/ReLibrary/Model/Except.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ReLibrary.View;
 
 namespace ReLibrary.Model
 {
@@ -13,37 +8,11 @@ namespace ReLibrary.Model
         {
             int menu = Constants.RESET;
             bool isError = true;
-
-            while(isError)
-            {
-                if(check == "1" || check == "2" || check == "3")
-                {
-                    menu = int.Parse(check);
-                    isError =Constants.NOERROR;
-                }
-
-                else
-                {
-                    Console.Clear();
-                    Common common = new Common();
-                    FirstPageScreen firstPage = new FirstPageScreen();
-                    common.PrintPageError();
-                    common.PrintInput();
-                    check = Console.ReadLine();
-                }
-            }
-
-            return menu; 
-        }
-
-        public int HandleUserloginExcept(string check)
-        {
-            int menu = Constants.RESET;
-            bool isError = true;
+            Screen screen = new Screen();
 
             while (isError)
             {
-                if (check == "1" || check == "2" || check == "3" || check == "4"|| check == "5")
+                if (check == "1" || check == "2" || check == "3")
                 {
                     menu = int.Parse(check);
                     isError = Constants.NOERROR;
@@ -52,15 +21,116 @@ namespace ReLibrary.Model
                 else
                 {
                     Console.Clear();
-                    Common common = new Common();
-                    UserLoginScreen userLoginScreen = new UserLoginScreen();
-                    common.PrintPageError();
-                    common.PrintInput();
+                    Console.Clear();
+                    screen.PrintLabel();
+                    screen.PrintFirstPage();
+                    screen.PrintPageError();
+                    screen.PrintInput();
                     check = Console.ReadLine();
                 }
             }
 
             return menu;
         }
+
+        public int HandleUserMenuExcept(string check)
+        {
+            int menu = Constants.RESET;
+            bool isError = true;
+            Screen screen = new Screen();
+
+            while (isError)
+            {
+                if (check == "1" || check == "2" || check == "3" || check == "4" || check == "5")
+                {
+                    menu = int.Parse(check);
+                    isError = Constants.NOERROR;
+                }
+
+                else
+                {
+                    Console.Clear();
+                    screen.PrintLabel();
+                    screen.PrintUserLoginPage();
+                    screen.PrintPageError();
+                    screen.PrintInput();
+                    check = Console.ReadLine();
+                }
+            }
+
+            return menu;
+        }
+
+        public string HandleUserNameExcept(string name)
+        {
+            bool isError = true;
+
+            while (isError)
+            {
+                if (name.Length >= 2 && name.Length <= 4)
+                {
+                    for (int index = 0; index < name.Length; index++)
+                    {
+                        if ((0xAC00 > name[index] || name[index] > 0xD7A3) || (0x3131 > name[index] && name[index] > 0x318E))
+                        {
+                            Screen screen = new Screen();
+                            screen.PrintNameError();
+                            name = Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            isError = Constants.NOERROR;
+                        }
+                    }
+                }
+
+                else
+                {
+                    Screen screen = new Screen();
+                    screen.PrintNameError();
+                    name = Console.ReadLine();
+                }
+            }
+            return name;
+        }
+
+        public string HandlePhoneNumberExcept(string phoneNumber,string name)
+        {
+            bool isError = true;
+            Screen screen = new Screen();
+
+            while(isError)
+            {
+                if(phoneNumber.Length == 11)
+                {
+                    for(int index = 0; index < phoneNumber.Length; index++)
+                    {
+                        if(phoneNumber[index] != '1' && phoneNumber[index] != '2' && phoneNumber[index] != '3' &&
+                           phoneNumber[index] != '4' && phoneNumber[index] != '5' && phoneNumber[index] != '6' &&
+                           phoneNumber[index] != '7' && phoneNumber[index] != '8' && phoneNumber[index] != '9' && phoneNumber[index] != '0')
+                        {
+                            screen.PrintPhoneNumberError(name);
+                            phoneNumber = Console.ReadLine();
+                        }
+
+                        else
+                        {
+                            isError = Constants.NOERROR;
+                        }
+                    }
+                }
+
+                else
+                {
+                    screen.PrintPhoneNumberError(name);
+                    phoneNumber = Console.ReadLine();
+                }
+            }
+
+            return phoneNumber;
+        }
+
     }
+
 }
