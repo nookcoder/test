fdbdad97b63b29889c155b2f13056665097e03ab
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-16 21:52:00 +0900

    [ADD] Book value object 생성

---
+++ ReLibrary/ReLibrary/VO/BooksVO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLibrary.VO
+{
+    class BooksVO
+    {
+        private string bookNumber;
+        private string title;
+        private string author;
+        private string publisher;
+
+        public BooksVO(string bookNumber,string title, string author, string publisher)
+        {
+            this.bookNumber = bookNumber;
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+        }
+
+        public string BookNumber
+        {
+            get { return bookNumber; }
+            set { bookNumber = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string Publisher
+        {
+            get { return publisher; }
+            set { publisher = value; }
+        }
+    }
+}
