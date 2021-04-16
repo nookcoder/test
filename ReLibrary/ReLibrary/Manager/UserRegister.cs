eebb5b63422c386a0349230cd7566c5a4b0aa8d6
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-16 21:53:27 +0900

    [ADD] 관리자가 회원을 등록시키는 기능 추가

---
+++ ReLibrary/ReLibrary/Manager/UserRegister.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.VO;
+using ReLibrary.Controller;
+
+namespace ReLibrary.Manager
+{
+    class UserRegister
+    {
+        List<UserVO> userList;
+        List<BooksVO> bookList;
+
+        public UserRegister(List<UserVO> userList, List<BooksVO> bookList)
+        {
+            this.userList = userList;
+            this.bookList = bookList;
+        }
+
+        public void RegisterUser()
+        {
+            SignUp signUp = new SignUp(userList, bookList);
+
+            string name;
+            string phoneNumber;
+            string address;
+            string id;
+            string password;
+            string hint;
+
+            name = signUp.InputName();
+            phoneNumber = signUp.InputNumber(name);
+            address = signUp.InputAdress(name, phoneNumber);
+            id = signUp.InputId(name, phoneNumber, address);
+            password = signUp.InputPassword(name, phoneNumber, address, id);
+            hint = signUp.InputHint(name, phoneNumber, address, id, password);
+
+            UserVO user = new UserVO(name, phoneNumber, address, id, password, hint);
+            userList.Add(user);
+
+            Console.Clear();
+
+            ManagerMenu managerMenu = new ManagerMenu(userList, bookList);
+            managerMenu.LoadMangerMenu();
+        }
+    }
+}
