9a171bae810910767f7ca4f6f0e915ecc78207fb
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-16 19:08:21 +0900

    [ADD] 초기 List 값 할당

--- ReLibrary/ReLibrary/Set.cs
+++ ReLibrary/ReLibrary/Set.cs
@@ -11,11 +11,23 @@ namespace ReLibrary.Model
     class Set
     {
         List<UserVO> userList;
+        List<BooksVO> bookList;
+        
         public Set()
         {
             this.userList = new List<UserVO>();
-            FirstPage firstPage = new FirstPage(this.userList);
-            
+            this.bookList = new List<BooksVO>();
+            if (this.userList.Count == 0)
+            {
+                this.userList.Add(new UserVO("김현욱", "01050477361", "사우", "qwe123qwe123", "qwe123qwe123", "김포고"));
+            }
+            if(this.bookList.Count == 0)
+            {
+                this.bookList.Add(new BooksVO("1", "컴포즈커피", "초코라떼", "휘핑추가"));
+                this.bookList.Add(new BooksVO("2", "빽다방", "아이스", "아메리카노"));
+                this.bookList.Add(new BooksVO("3", "오븐속", "파스타", "크림파스타") );
+            }
+            FirstPage firstPage = new FirstPage(this.userList,this.bookList);
         }
     }
 }
