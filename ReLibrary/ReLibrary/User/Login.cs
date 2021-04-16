dcc72584ce1fa9ecc555b5b9f088f92ab3c57b75
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-17 01:31:30 +0900

    [ADD] 회원 로그인 기능 추가

---
+++ ReLibrary/ReLibrary/User/Login.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.VO;
+using ReLibrary.User;
+
+namespace ReLibrary.Controller
+{
+    class Login
+    {
+        List<UserVO> userList;
+        List<BooksVO> bookList;
+        public Login(List<UserVO> userList, List<BooksVO> bookList)
+        {
+            this.bookList = bookList;
+            this.userList = userList;
+        }
+        
+        public void TryLogin()
+        {
+            string Id;
+            string password;
+
+            Screen screen = new Screen();
+            Console.Clear();
+            screen.PrintLoginId(null);
+            Id = Console.ReadLine();
+            screen.PrintSignUpPassword();
+            password = Console.ReadLine();
+            CheckIdPassword(Id, password);
+        }
+
+        public void CheckIdPassword(string id,string password)
+        {
+            bool isFound = false; 
+
+            for(int index = 0; index < userList.Count; index++)
+            {
+                if(userList[index].Id == id && userList[index].Password == password)
+                {
+                    isFound = true;
+                }
+            }
+
+            if(isFound)
+            {
+                UserBookMenu userBookMenu = new UserBookMenu(userList,bookList);
+                Console.Clear();
+                userBookMenu.LoadUserBookMenu();
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" ");
+                Console.WriteLine("           아이디 혹은 비밀번호가 일치하지않습니다.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                UserLoginPage userLogin = new UserLoginPage(userList,bookList);
+                userLogin.GoUserLoginNextMenu();
+            }
+        }
+    }
+}
