c67c9f58efe63f1a33533120641da7dad7cc2855
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-16 15:16:10 +0900

    [ADD] 초기설정 클래스 추가

---
+++ ReLibrary/ReLibrary/Set.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReLibrary.VO;
+using ReLibrary.Controller;
+
+namespace ReLibrary.Model
+{
+    class Set
+    {
+        List<UserVO> userList;
+        public Set()
+        {
+            this.userList = new List<UserVO>();
+            FirstPage firstPage = new FirstPage(this.userList);
+            
+        }
+    }
+}
