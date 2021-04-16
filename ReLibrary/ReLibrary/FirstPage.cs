da2672ba3158b260cce2b4a23eaa7a0035b55d2a
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-16 19:09:18 +0900

    [ADD] 실행 초기화면 생성

---
+++ ReLibrary/ReLibrary/FirstPage.cs
@@ -0,0 +1,65 @@
+﻿using ReLibrary.Model;
+using ReLibrary.VO;
+using System;
+using System.Collections.Generic;
+using ReLibrary.Manager;
+
+namespace ReLibrary.Controller
+{
+    class FirstPage
+    {
+        List<BooksVO> bookList;
+        List<UserVO> userList;
+        public FirstPage(List<UserVO> userList, List<BooksVO> bookList)
+        {
+            this.bookList = bookList;
+            this.userList = userList;
+
+            Console.SetWindowSize(60, 30);
+            ShowFirstPage();
+        }
+
+        public void ShowFirstPage()
+        {
+            Screen screen = new Screen();
+            screen.PrintLabel();
+            screen.PrintFirstPage();
+            screen.PrintInput();
+            GoSecondPage();
+        }
+
+        public void GoSecondPage()
+        {
+            Except except = new Except();
+
+            string check;
+            int menu;
+
+            check = Console.ReadLine();
+            menu = except.HandleFirstMenuExcept(check);
+            GuideFirstPageMenu(menu);
+        }
+
+        public void GuideFirstPageMenu(int menu)
+        {
+            switch (menu)
+            {
+                case Constants.USER_MENU:
+                    UserLoginPage userLogin = new UserLoginPage(this.userList, this.bookList);
+                    userLogin.GoUserLoginNextMenu();
+                    break;
+
+                case Constants.MANAGER_MENU:
+                    ManagerLoginPage managerLoginPage = new ManagerLoginPage(userList, bookList);
+                    managerLoginPage.TryManagerLogin();
+                    break;
+
+                case Constants.QUIT:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+    }
+
+}
