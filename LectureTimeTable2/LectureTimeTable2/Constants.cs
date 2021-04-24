acc174b870a24001e495133629315e9ebd879176
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-25 01:02:20 +0900

    [ADD] LoginPage Constants 추가

---
+++ LectureTimeTable2/LectureTimeTable2/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureTimeTable2
+{
+    class Constants
+    {
+        // bool형 Constants
+        public const bool ERROR = true; 
+        public const bool NOERROR = false;
+        public const bool NONE = true;
+        public const bool FIND = false;
+
+        // InitialMenu Constants
+        public const int LECTURETIMETABLE = 1;
+        public const int ATTENTION = 2;
+        public const int APPLICATION = 3;
+        public const int CHECKINGCOURSE = 4;
+        public const int CHECKINGTIMETABLE = 5;
+        public const int Exit = 6;
+
+    }
+}
