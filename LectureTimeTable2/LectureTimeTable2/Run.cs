abaeaacaf74ec1c379d2b78365c2844096f00f1d
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-25 01:02:55 +0900

    [ADD] main 프로그램에서 실행할 클래스 추가

---
+++ LectureTimeTable2/LectureTimeTable2/Run.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LectureTimeTable2.VOs;
+using LectureTimeTable2.Login;
+
+namespace LectureTimeTable2
+{
+    class Run
+    {
+        List<StudentsVO> students;
+        List<CoursesVO> courses;
+        List<StudentCourseVO> studentCourses;
+       
+        public Run()
+        {
+            Console.SetWindowSize(100, 40);
+
+            this.students = new List<StudentsVO>();
+            this.courses = new List<CoursesVO>();
+            this.studentCourses = new List<StudentCourseVO>();
+            students.Add(new StudentsVO("18011250", "ensharp21"));
+            RunPrrogram();
+        }
+
+        public void RunPrrogram()
+        {
+            Login.Login loginPractice = new Login.Login(students, courses, studentCourses);
+            loginPractice.RunLogin();
+        }
+    }
+}
