1a404842ea5df449b1098dc5b074ffeb450dd7a4
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-25 01:03:59 +0900

    [ADD] 로그인 관련 UI 추가

---
+++ LectureTimeTable2/LectureTimeTable2/LoginProperty/LoginScreen.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace LectureTimeTable2.Login
+{
+    class LoginScreen
+    {
+        public void PrintLTT()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(String.Format("{0,50}", "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"));
+            Console.WriteLine(String.Format("{0,82}", "■                                                                ■"));
+            Console.WriteLine(String.Format("{0,68}", "■   ■■                 □□□□□□             ■■■■■■   ■"));
+            Console.WriteLine(String.Format("{0,68}", "■   ■■                 □□□□□□             ■■■■■■   ■"));
+            Console.WriteLine(String.Format("{0,76}", "■   ■■                     □□                     ■■       ■"));
+            Console.WriteLine(String.Format("{0,76}", "■   ■■                     □□                     ■■       ■"));
+            Console.WriteLine(String.Format("{0,76}", "■   ■■                     □□                     ■■       ■"));
+            Console.WriteLine(String.Format("{0,73}", "■   ■■■■■               □□                     ■■       ■"));
+            Console.WriteLine(String.Format("{0,73}", "■   ■■■■■               □□                     ■■       ■"));
+            Console.WriteLine(String.Format("{0,82}", "■                                                                ■"));
+            Console.WriteLine(String.Format("{0,50}", "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"));
+            Console.WriteLine(String.Format("{0,52}", "■■■■■■■■■종료하시려면 q를 입력해주세요 ■■■■■■■■■■"));
+            Console.WriteLine(String.Format("{0,50}", "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        // 학번 입력 받기
+        public void PrintId(string id)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,39}", " "));
+            Console.Write($"학번 : {id}");
+        }
+
+        // 비밀번호 입력 받기
+        public void PrintPassword(string password)
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,39}", " "));
+            Console.Write($"비밀번호 : {password}");
+        }
+
+        // 학번 입력 오류시 
+        public void PrintReWriteId()
+        {
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(String.Format("{0,46}", "다시입력해주세요"));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(String.Format("{0,39}", " "));
+            Console.Write($"학번 : ");
+        }
+
+        // 비밀번호 입력 오류 시
+        public void PrintReWritePassword()
+        {
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(String.Format("{0,53}", "8자리이상 12자리 이하 숫자/영어조합만 입력해주세요"));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(String.Format("{0,39}", " "));
+            Console.Write($"비밀번호 : ");
+        }
+
+        // 로그인 실패 
+        public void PrintNoExistId()
+        {
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(String.Format("{0,50}","가입하지 않은 아이디이거나,잘못된 비밀번호입니다."));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(String.Format("{0,50}","계속 진행하시려면 아무키나 눌러주세요."));
+        }
+
+        // 로그인 후 띄울 메뉴창 
+        public void PrintInitialMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,20}", " "));
+            Console.Write("============================================================");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[1] 강의 시간표");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[2] 관심 과목 담기");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[3] 수강 신청");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[4] 수강 내역 조회");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[5] 시간표 조회");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("[6] 종료 하기");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,20}", " "));
+            Console.Write("============================================================");
+        }
+
+        // 메뉴 입력 받기 
+        public void PrintInitialMenuInput()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("\n");
+            Console.Write(String.Format("{0,41}", " "));
+            Console.Write("입력 : ");
+        }
+
+        // 메뉴 입력 오류 시 
+        public void PrintMenuError()
+        {
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(String.Format("{0,41}", " "));
+            Console.WriteLine("잘못 입력하셨습니다.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(String.Format("{0,41}", " "));
+            Console.Write("입력 : ");
+        }
+    }
+}
