c410ee016c117fa3f02e35e23928956546b77c5f
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-26 03:04:53 +0900

    [ADD] 학생 VO 클래스 추가

---
+++ LectureTimeTable2/LectureTimeTable2/VOs/StudentCourseVO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureTimeTable2.VOs
+{
+    class StudentCourseVO
+    {
+        List<StudentsVO> studentsVOs;
+        List<LecturesVO> lecturesVOs;
+
+        public StudentCourseVO(List<StudentsVO> studentsVOs, List<LecturesVO> coursesVOs)
+        {
+            this.studentsVOs = studentsVOs;
+            this.lecturesVOs = coursesVOs;
+        }
+
+        public StudentCourseVO(List<StudentsVO> studentsVOs)
+        {
+            this.studentsVOs = studentsVOs;
+        }
+
+        public StudentCourseVO(List<LecturesVO> lecturesVOs)
+        {
+            this.lecturesVOs = lecturesVOs;
+        }
+
+        public List<StudentsVO> Students
+        {
+            get { return studentsVOs; }
+            set { studentsVOs = value; }
+        }
+    }
+}
