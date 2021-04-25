fb37ef4f99b1060b4a352b779456b918dec6fa38
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-26 03:07:10 +0900

    [ADD] LectureMenu 관련 예외처리 기능 추가

---
+++ LectureTimeTable2/LectureTimeTable2/LectureProperty/LectureException.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace LectureTimeTable2.LectureProperty
+{
+    class LectureException:LectureScreen
+    {
+        // 강의 검색 방법 선택 메뉴창에서 예외처리 
+        public string HandleLectureTimeTableMenuInput(string menuCheck)
+        {
+            while (Constants.ERROR)
+            {
+                if (Regex.IsMatch(menuCheck, @"^[1-6]$"))
+                {
+                    break;
+                }
+
+                PrintLectureTimeTableMenu();
+                PrintMenuError();
+                menuCheck = Console.ReadLine();
+            }
+
+            return menuCheck;
+        }
+
+        // 전공으로 검색에서 나오는 메뉴 예외처리 
+        public string HandleSearchByMajor(string menuCheck)
+        {
+            while (Constants.ERROR)
+            {
+                if (Regex.IsMatch(menuCheck, @"^[1-4]$"))
+                {
+                    break;
+                }
+
+                PrintLectureTimeTableMenu();
+                PrintMenuError();
+                menuCheck = Console.ReadLine();
+            }
+
+            return menuCheck;
+        }
+    }
+}
