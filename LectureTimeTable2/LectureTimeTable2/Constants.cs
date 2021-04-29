b5ef95b13f1e95035fc94a5466a2db594da72124
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-30 02:26:38 +0900

    [ADD] 수강 신청관련 Constants 추가

--- LectureTimeTable2/LectureTimeTable2/Constants.cs
+++ LectureTimeTable2/LectureTimeTable2/Constants.cs
@@ -18,9 +18,8 @@ namespace LectureTimeTable2
         public const int LECTURETIMETABLE = 1;
         public const int ATTENTION = 2;
         public const int REGISTRATION = 3;
-        public const int CHECKINGCOURSE = 4;
-        public const int CHECKINGTIMETABLE = 5;
-        public const int Exit = 6;
+        public const int CHECKINGTIMETABLE = 4;
+        public const int Exit = 5;
 
         // LectureTimeTableMenu Constants
         public const int MAJOR = 1;
@@ -42,7 +41,18 @@ namespace LectureTimeTable2
         public const int INQUIRY = 2;
         public const int BACK = 3;
 
+        // 수강 신청 메뉴 
         public const int ATTENTIONREGISTER = 1;
+        public const int REGISTER_BACK = 4; 
+
+        // 시간이 겹치는 지 판단
+        public const int ONETIME = 8;
+        public const int TWOTIME = 16;
+        public const bool DAY_NOFIND = false;
+        public const bool DAY_FIND = true;
+        public const bool DO_REGISTER = true;
+        public const bool NO_REGISTER = false;
+
 
     }
 }
