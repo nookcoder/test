2ab94602fb475df600b46315333e9ac58ce53864
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-29 16:26:32 +0900

    [ADD] 수강신청 VO 추가

---
+++ LectureTimeTable2/LectureTimeTable2/VOs/RegistrationVO.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureTimeTable2.VOs
+{
+    class RegistrationVO
+    {
+        private string major;
+        private string courseNumber;
+        private string distribution;
+        private string title;
+        private string sortation;
+        private string grade;
+        private int credit;
+        private string courseTime;
+        private string classRoom;
+        private string professor;
+        private string language;
+
+        public RegistrationVO(object major, object courseNumber, object distribution, object title, object sortation,
+                         object grade, object credit, object courseTime, object classRoom, object professor, object language)
+        {
+            this.major = major.ToString();
+            this.courseNumber = courseNumber.ToString();
+            this.distribution = distribution.ToString();
+            this.title = title.ToString();
+            this.sortation = sortation.ToString();
+            this.grade = grade.ToString();
+            this.credit = Convert.ToInt32(credit);
+            this.courseTime = courseTime.ToString();
+            this.classRoom = Convert.ToString(classRoom);
+            this.professor = Convert.ToString(professor);
+            this.language = language.ToString();
+        }
+
+        public string Major
+        {
+            get { return major; }
+            set { major = value; }
+        }
+
+        public string CourseNumber
+        {
+            get { return courseNumber; }
+            set { courseNumber = value; }
+        }
+
+        public string Distribution
+        {
+            get { return distribution; }
+            set { distribution = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Sortation
+        {
+            get { return sortation; }
+            set { sortation = value; }
+        }
+
+        public string Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+        public int Credit
+        {
+            get { return credit; }
+            set { credit = value; }
+        }
+
+        public string CourseTime
+        {
+            get { return courseTime; }
+            set { courseTime = value; }
+        }
+
+        public string ClassRoom
+        {
+            get { return classRoom; }
+            set { classRoom = value; }
+        }
+
+        public string Professor
+        {
+            get { return professor; }
+            set { professor = value; }
+        }
+        public string Language
+        {
+            get { return language; }
+            set { language = value; }
+        }
+    }
+}
+
