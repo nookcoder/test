6634d83bb124aa4b7093353f4c09e3789b1d9654
Hyeon Wook Kim <gus70005319@gmail.com>
2021-04-30 02:30:00 +0900

    [ADD] 수강 신청 메뉴 관련 예외처리 기능

---
+++ LectureTimeTable2/LectureTimeTable2/Registration/RegistrationException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LectureTimeTable2.Registeration
+{
+    class RegistrationException
+    {
+        private RegistrationScreen RegistrationScreen; 
+
+        public RegistrationException()
+        {
+            this.RegistrationScreen = new RegistrationScreen();
+        }
+
+        public int HandleGetRegistrationMenu(string menuCheck)
+        {
+            int menu;
+            
+            Regex regex = new Regex(@"^[1-4]{1}$");
+            
+            while(!regex.IsMatch(menuCheck))
+            {
+                RegistrationScreen.PrintRegistrationMenu();
+                RegistrationScreen.PrintMenuError();
+                menuCheck = Console.ReadLine();
+            }
+
+            menu = Convert.ToInt32(menuCheck);
+    
+            return menu;
+        }
+
+        public string HandleGetReviseAttentionCourse(string attentionCourseNubmerCheck)
+        {
+            Regex regex = new Regex(@"^[q1-9]{1,2}$");
+
+            while (!regex.IsMatch(attentionCourseNubmerCheck))
+            {
+                RegistrationScreen.PrintRegisterCourseNumberError();
+                attentionCourseNubmerCheck = Console.ReadLine();
+            }
+
+            return attentionCourseNubmerCheck;
+        }
+    }
+}
