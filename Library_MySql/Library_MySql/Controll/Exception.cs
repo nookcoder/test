189bfcb7e98ecadfc20729504efe60f4936716f1
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 04:21:36 +0900

    [ADD] 전화번호 예외처리, 주소 예외처리 추가

--- Library_MySql/Library_MySql/Controll/Exception.cs
+++ Library_MySql/Library_MySql/Controll/Exception.cs
@@ -23,11 +23,11 @@ namespace Library_MySql
             return exception;
         }
 
-        public string HandleGetId(string idCheck)
+        public string HandleGetId(string check)
         {
             Regex regex = new Regex(@"^[0-9a-zA-Z]");
 
-            while (!regex.IsMatch(idCheck) || idCheck == null)
+            while (!regex.IsMatch(check) || check?.Length == 0)
             {
                 Console.SetCursorPosition(0, 3);
                 Console.Write(new String(' ', 1000));
@@ -36,20 +36,20 @@ namespace Library_MySql
                 Console.SetCursorPosition(0, 5);
                 Initialization.screen.PrintInputError();
                 Console.SetCursorPosition(24, 4);
-                idCheck = Console.ReadLine();
+                check = Console.ReadLine();
             }
             Console.SetCursorPosition(0, 5);
             Console.Write(new String(' ', 1000));
             Console.SetCursorPosition(0, 4);
 
-            return idCheck;
+            return check;
         }
 
-        public string HandleGetPassword(string idCheck)
+        public string HandleGetPassword(string check)
         {
             Regex regex = new Regex(@"^[0-9a-zA-Z]");
 
-            while (!regex.IsMatch(idCheck) || idCheck == null)
+            while (!regex.IsMatch(check) || check == null)
             {
                 Console.SetCursorPosition(0, 7);
                 Console.Write(new String(' ', 1000));
@@ -57,14 +57,81 @@ namespace Library_MySql
                 Initialization.screen.PrintGetPassword();
                 Console.SetCursorPosition(0, 9);
                 Initialization.screen.PrintInputError();
-                Console.SetCursorPosition(24, 8);
-                idCheck = Console.ReadLine();
+                Console.SetCursorPosition(26, 8);
+                check = Console.ReadLine();
             }
             Console.SetCursorPosition(0, 9);
             Console.Write(new String(' ', 1000));
-            Console.SetCursorPosition(0, 9);
+            Console.SetCursorPosition(0, 8);
 
-            return idCheck;
+            return check;
         }
+
+        public string HandleGetName(string check)
+        {
+            Regex regex = new Regex(@"^[가-힣a-zA-Z]{2,}");
+
+            while (!regex.IsMatch(check) || check == null)
+            {
+                Console.SetCursorPosition(0, 11);
+                Console.Write(new String(' ', 1000));
+                Console.SetCursorPosition(0, 8);
+                Initialization.screen.PrintGetName();
+                Console.SetCursorPosition(0, 13);
+                Initialization.screen.PrintInputError();
+                Console.SetCursorPosition(9, 12);
+                check = Console.ReadLine();
+            }
+            Console.SetCursorPosition(0, 13);
+            Console.Write(new String(' ', 1000));
+            Console.SetCursorPosition(0, 12);
+
+            return check;
+        }
+        public string HandleGetPhoneNumber(string check)
+        {
+            Regex regex = new Regex(@"^(010)(\d{4})(\d)");
+            Regex regex1 = new Regex(@"^(011)(\d{4})(\d)");
+            while (!regex.IsMatch(check) || regex1.IsMatch(check))
+            {
+                Console.SetCursorPosition(0, 15);
+                Console.Write(new String(' ', 1000));
+                Console.SetCursorPosition(0, 12);
+                Initialization.screen.PrintGetPhoneNumber();
+                Console.SetCursorPosition(0, 17);
+                Initialization.screen.PrintInputError();
+                Console.SetCursorPosition(23, 16);
+                check = Console.ReadLine();
+            }
+            Console.SetCursorPosition(0, 17);
+            Console.Write(new String(' ', 1000));
+            Console.SetCursorPosition(0, 16);
+
+            return check;
+        }
+
+        public string HandleGetAddress(string check)
+        {
+            Regex regex = new Regex(@"([가-힣]+)시 ([가-힣0-9]+)대로");
+            Regex regex1 = new Regex(@"([가-힣]+)시 ([가-힣0-9]+)로");
+            Regex regex2 = new Regex(@"([가-힣]+)시 ([가-힣0-9]+)길");
+            while (!regex.IsMatch(check) && !regex1.IsMatch(check) && !regex2.IsMatch(check))
+            {
+                Console.SetCursorPosition(0, 19);
+                Console.Write(new String(' ', 1000));
+                Console.SetCursorPosition(0, 16);
+                Initialization.screen.PrintGetAddress();
+                Console.SetCursorPosition(0, 21);
+                Initialization.screen.PrintInputError();
+                Console.SetCursorPosition(28, 20);
+                check = Console.ReadLine();
+            }
+            Console.SetCursorPosition(0, 21);
+            Console.Write(new String(' ', 1000));
+            Console.SetCursorPosition(0, 20);
+
+            return check;
+        }
+
     }
 }
