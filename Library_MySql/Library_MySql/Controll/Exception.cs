918c3a102a8a136d191ce8baf3d5e9e13bb3987d
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 03:33:23 +0900

    [ADD] 아이디, 비밀번호 예외처리 기능 추가

---
+++ Library_MySql/Library_MySql/Controll/Exception.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Library_MySql
+{
+    class Exception
+    {
+        private Exception() { }
+
+        private static Exception exception = null;
+
+        public static Exception Instance()
+        {
+            if (exception == null)
+            {
+                exception = new Exception();
+            }
+
+            return exception;
+        }
+
+        public string HandleGetId(string idCheck)
+        {
+            Regex regex = new Regex(@"^[0-9a-zA-Z]");
+
+            while (!regex.IsMatch(idCheck) || idCheck == null)
+            {
+                Console.SetCursorPosition(0, 3);
+                Console.Write(new String(' ', 1000));
+                Console.SetCursorPosition(0, 0);
+                Initialization.screen.PrintGetId();
+                Console.SetCursorPosition(0, 5);
+                Initialization.screen.PrintInputError();
+                Console.SetCursorPosition(24, 4);
+                idCheck = Console.ReadLine();
+            }
+            Console.SetCursorPosition(0, 5);
+            Console.Write(new String(' ', 1000));
+            Console.SetCursorPosition(0, 4);
+
+            return idCheck;
+        }
+
+        public string HandleGetPassword(string idCheck)
+        {
+            Regex regex = new Regex(@"^[0-9a-zA-Z]");
+
+            while (!regex.IsMatch(idCheck) || idCheck == null)
+            {
+                Console.SetCursorPosition(0, 7);
+                Console.Write(new String(' ', 1000));
+                Console.SetCursorPosition(0, 4);
+                Initialization.screen.PrintGetPassword();
+                Console.SetCursorPosition(0, 9);
+                Initialization.screen.PrintInputError();
+                Console.SetCursorPosition(24, 8);
+                idCheck = Console.ReadLine();
+            }
+            Console.SetCursorPosition(0, 9);
+            Console.Write(new String(' ', 1000));
+            Console.SetCursorPosition(0, 9);
+
+            return idCheck;
+        }
+    }
+}
