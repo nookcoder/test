565550f6a818faf541fbdaf2431b1d10efc8490a
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 03:32:26 +0900

    [ADD] Screen, Exception 초기화, 초기메뉴 const 추가

---
+++ Library_MySql/Library_MySql/Initialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_MySql
+{
+    class Initialization
+    {
+        public static Screen screen = Screen.Instance();
+        public static Exception exception = Exception.Instance();
+
+        public enum InitalMenu
+        {
+            MEMBERLOGIN = 1,
+            JOIN,
+            MANAGERLOGIN,
+            EXIT
+        }
+
+
+
+    }
+}
