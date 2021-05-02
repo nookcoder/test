95e5f1114f62edfdc2250f48c6de30b4d6edeb23
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 03:33:57 +0900

    [ADD] 회원가입 class 추가, id passwrod 얻어오는 기능 추가

---
+++ Library_MySql/Library_MySql/Controll/Join.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_MySql.Controll
+{
+    class Join
+    {
+        public Join()
+        {
+
+        }
+
+        public string Getid()
+        {
+            string idCheck;
+            string id;
+            Initialization.screen.PrintGetId();
+            idCheck = Console.ReadLine();
+            id = Initialization.exception.HandleGetId(idCheck);
+
+            return id;
+        }
+
+        public string GetPassword()
+        {
+            string passwordCheck;
+            string password;
+            Initialization.screen.PrintGetPassword();
+            passwordCheck = Console.ReadLine();
+            password = Initialization.exception.HandleGetPassword(passwordCheck);
+
+            return password;
+        }
+
+        public string GetName()
+        {
+            string nameCheck;
+            string name;
+            Initialization.screen.PrintGetPassword();
+            nameCheck = Console.ReadLine();
+            name = Initialization.exception.HandleGetPassword(nameCheck);
+
+            return name;
+        }
+    }
+}
