88b20bde2ad39c1909480a04cf586d839149001c
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-04 00:50:01 +0900

    [ADD] 도서 이름 검색 기능 추가 중

--- Library_MySql/Library_MySql/Controll/Search.cs
+++ Library_MySql/Library_MySql/Controll/Search.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,45 @@ namespace Library_MySql.Controll
 {
     class Search
     {
+        public Search()
+        {
+
+        }
+
+        public string GetTitle()
+        {
+            string Title;
+
+            Console.Clear();
+            Initialization.screen.PrintGetBookTitle();
+            Title = Console.ReadLine();
+
+            return Title;
+
+        }
+
+        public void SearchByTitle()
+        {
+            string title;
+            string connStr = "Server=localhost;Database=member;Uid=root;Pwd=0000;Charset=utf8";
+
+            title = GetTitle();
+
+            using (MySqlConnection connection = new MySqlConnection(connStr))
+            {
+                connection.Open();
+                string sql = "SELECT * FROM book";
+                MySqlCommand cmd = new MySqlCommand(sql, connection);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    if (reader["bookTitle"].ToString().Contains(title))
+                    {
+                        Console.WriteLine($"{reader["bookTitle"]}");
+                    }
+                }
+                reader.Close();
+            }
+        }
     }
 }
