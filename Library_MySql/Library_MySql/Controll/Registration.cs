62b1502ee3ac58b6fd9cd05833ef0687fef37ea4
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 13:45:35 +0900

    [ADD] class 이렇게!!

---
+++ Library_MySql/Library_MySql/Controll/Registration.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library_MySql.Model;
+
+namespace Library_MySql.Controll
+{
+    class Registration
+    {
+        private MemberData memberData;
+
+        public Registration()
+        {
+            this.memberData = new MemberData();
+        }
+
+        public void RunRegisterMember()
+        {
+            string id, password, name, phoneNumber, address;
+            id = GetMemberid();
+            password = GetMemberPassword();
+            name = GetMemberName();
+            phoneNumber = GetMemberPhoneNumber();
+            address = GetMemberAddress();
+
+            memberData.InsertMemberData(id, password, name, phoneNumber, address, null);
+        }
+
+        public string GetMemberid()
+        {
+            string idCheck;
+            string id;
+            Initialization.screen.PrintGetId();
+            idCheck = Console.ReadLine();
+            id = Initialization.exception.HandleGetId(idCheck,memberData);
+
+            return id;
+        }
+
+        public string GetMemberPassword()
+        {
+            string passwordCheck;
+            string password;
+            Initialization.screen.PrintGetPassword();
+            passwordCheck = Console.ReadLine();
+            password = Initialization.exception.HandleGetPassword(passwordCheck);
+
+            return password;
+        }
+
+        public string GetMemberName()
+        {
+            string nameCheck;
+            string name;
+            Initialization.screen.PrintGetName();
+            nameCheck = Console.ReadLine();
+            name = Initialization.exception.HandleGetName(nameCheck);
+
+            return name;
+        }
+
+        public string GetMemberPhoneNumber()
+        {
+            string phoneNumberCheck;
+            string phoneNumber;
+            Initialization.screen.PrintGetPhoneNumber();
+            phoneNumberCheck = Console.ReadLine();
+            phoneNumber = Initialization.exception.HandleGetPhoneNumber(phoneNumberCheck,memberData);
+
+            return phoneNumber;
+        }
+
+        public string GetMemberAddress()
+        {
+            string addressCheck;
+            string address;
+            Initialization.screen.PrintGetAddress();
+            addressCheck = Console.ReadLine();
+            address = Initialization.exception.HandleGetAddress(addressCheck);
+
+            return address;
+        }
+    }
+}
