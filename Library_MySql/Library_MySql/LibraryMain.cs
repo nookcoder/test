3a1f0bf3fcc81e56690f2e2de04165b207a804bb
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-03 13:46:07 +0900

    [ADD] 메인함수 겸 테스트!!

---
+++ Library_MySql/Library_MySql/LibraryMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Library_MySql.Controll;
+
+namespace Library_MySql
+{
+    class LibraryMain
+    {
+        static void Main(string[] args)
+        {
+            InitialMenu initialMenu = new InitialMenu();
+            Registration join = new Registration();
+            initialMenu.RunMenu(join);
+        }
+    }
+}
