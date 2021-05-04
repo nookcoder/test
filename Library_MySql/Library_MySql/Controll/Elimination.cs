5193847d27a2f9e88ac936db9c70c509beb7665e
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-04 11:53:18 +0900

    [ADD] 도서 삭제 기능 추가

--- Library_MySql/Library_MySql/Controll/Elimination.cs
+++ Library_MySql/Library_MySql/Controll/Elimination.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library_MySql.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,50 @@ namespace Library_MySql.Controll
 {
     class Elimination
     {
+        private BookData bookData; 
+
+        public Elimination()
+        {
+            this.bookData = new BookData();
+        }
+
+        public string GetBookId()
+        {
+            string bookIdCheck;
+            string bookId;
+
+            Console.Clear();
+            Initialization.screen.PrintExit();
+            Initialization.screen.PrintGetDeleteBookId();
+            bookIdCheck = Console.ReadLine();
+            bookId = Initialization.exception.HandleGetBookIdInModification(bookIdCheck);
+
+            return bookId;
+        }
+
+        public void RunDeletBook(BookData bookData)
+        {
+            string bookId;
+
+            bookId = GetBookId();
+
+            if (bookId != "q")
+            {
+                if(bookData.IsBookIdDuplication(bookId))
+                {
+                    bookData.DeletBookdata(bookId);
+                    Initialization.screen.PrintDeletingNotice();
+                    Initialization.screen.PrintNext();
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    Initialization.screen.PrintNoFindBookNOtice();
+                }
+            }
+
+            else { }
+        }
     }
 }
