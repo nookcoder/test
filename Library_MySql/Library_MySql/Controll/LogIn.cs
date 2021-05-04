b70156d22a1834f53310f3427410993097220b56
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-05 04:58:15 +0900

    [ADD] 로그인 기능 추가

---
+++ Library_MySql/Library_MySql/Controll/LogIn.cs
@@ -0,0 +1,119 @@
+﻿using Library_MySql.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_MySql.Controll
+{
+    class LogIn
+    {
+
+        public LogIn()
+        {
+
+        }
+
+        public string GetId()
+        {
+            string idCheck;
+            string id;
+
+            Initialization.screen.PrintExit();
+            Initialization.screen.PrintGetLoginId();
+            idCheck = Console.ReadLine();
+            id = Initialization.exception.HandleGetIdInLogin(idCheck);
+
+            return id;
+        }
+
+        public string GetPassword()
+        {
+            string passwordCheck;
+            string password;
+
+            Initialization.screen.PrintGetPassword();
+            passwordCheck = Console.ReadLine();
+            password = Initialization.exception.HandleGetPasswordInLogin(passwordCheck);
+
+            return password;
+        }
+
+        public string GetManagerCode()
+        {
+            string code;
+
+            Console.Clear();
+            Initialization.screen.PrintGetManagerCode();
+            code = Console.ReadLine();
+
+            return code;
+        }
+
+        public bool RunCheckMember(MemberData memberData)
+        {
+            string id;
+            string password;
+
+            bool isSuccess = false;
+
+            id = GetId();
+
+            if (id != "q")
+            {
+                password = GetPassword();
+                if (password != "q")
+                {
+                    if (memberData.IsMemberIdDuplication(id))
+                    {
+                        if (memberData.IsMemberPasswordDuplication(id, password))
+                        {
+                            Initialization.screen.PrintLoginSuccess();
+                            isSuccess = true;
+                        }
+
+                        else
+                        {
+                            Initialization.screen.PrintFailLogin();
+                        }
+                    }
+
+                    else
+                    {
+                        Initialization.screen.PrintFailLogin();
+                    }
+                }
+
+                else { }
+
+            }
+
+            else { }
+
+
+            return isSuccess;
+        }
+
+        public bool RunCheckManager()
+        {
+            bool isRight = false;
+            string code;
+
+            code = GetManagerCode();
+           
+            if(code == "*")
+            {
+                isRight = true;
+                Initialization.screen.PrintLoginSuccess();
+            }
+
+            else
+            {
+                Initialization.screen.PrintFailLogin();
+            }
+
+            return isRight;
+        }
+    }
+}
