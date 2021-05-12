369da3360e7b8b5ee79182f8c4157cfc47b75a3a
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-12 21:22:26 +0900

    [ADD] 이미지를 jfram 에 넣어보기

--- ImageSearch/src/main/Frame.java
+++ ImageSearch/src/main/Frame.java
@@ -1,26 +1,45 @@
 package main;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import main.KakaoCrawler;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import java.awt.*;
 import java.io.IOException;
+import java.net.URL;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
 public class Frame extends JFrame{
 	Container c; 
-	
-	public Frame() 
+	URL url;
+	JLabel label;
+	public Frame() throws ParseException, IOException 
 	{
 		setTitle("이미지 검색");
 		setSize(600,400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTextField();
-
+		setVisible(true);
 	}
 	
-	public void setTextField()
+	public void setTextField() throws ParseException, IOException
 	{
 		KakaoCrawler api = new KakaoCrawler();
+		JSONArray imgUrlArray = api.ImageParse();
 		c = getContentPane(); 
+		for(int i =0;i<imgUrlArray.size() ;i++)
+		{
+			JSONObject jsonobj = (JSONObject)imgUrlArray.get(i);
+			String imgUrl = (String)jsonobj.get("image_url");
+			url = new URL(imgUrl);
+			BufferedImage image = ImageIO.read(url);
+			label = new JLabel(new ImageIcon(image));
+			c.add(label);
+		}
+
 		c.setLayout(new FlowLayout());
 		c.add(new JLabel("검색 "));
 		c.add(new JTextField(20));
