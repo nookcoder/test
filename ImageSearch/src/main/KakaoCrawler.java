f585bbe559ff2c4efcbd1fa26a88c605063a63db
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-12 21:22:01 +0900

    [ADD] image url 받아오기

--- ImageSearch/src/main/KakaoCrawler.java
+++ ImageSearch/src/main/KakaoCrawler.java
@@ -1,16 +1,22 @@
 package main;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.util.jar.JarException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class KakaoCrawler {
 
@@ -19,10 +25,9 @@ public class KakaoCrawler {
 	 private static final String API_SERVER_HOST = "https://dapi.kakao.com";
 	 private static final String IMAGE_PATH = "/v2/search/image.json?query=";
 	 
-	 public void Get() throws IOException
+	 public String Get() throws IOException
 	 {
-		 byte[] image; 
-
+		 String str = null;
 		 try {
 	        	String query = URLEncoder.encode("°í¾çÀÌ","UTF-8");
 	            String apiURL = API_SERVER_HOST + IMAGE_PATH + query;
@@ -44,9 +49,24 @@ public class KakaoCrawler {
 	            }
 	            
 	            br.close();
-	            System.out.println(response.toString());
-	        } catch (Exception e) {
+	            str = response.toString();
+		 } catch (Exception e) {
 	            System.out.println(e);
 	        }
+		 
+		 return str;
 	 }
+	 
+	public JSONArray ImageParse() throws ParseException, IOException{
+		 JSONParser jsonParser = new JSONParser();
+		 JSONObject jsonObject = (JSONObject)jsonParser.parse(Get());
+		 JSONArray jsonArray = (JSONArray)jsonObject.get("documents");
+		 
+		 return jsonArray;
+//		 for(int i =0;i<jsonArray.size();i++)
+//		 {
+//			 JSONObject jsonobj = (JSONObject)jsonArray.get(i);
+//			 String imgUrl = (String)jsonobj.get("image_url");
+//		 }
+	}
 }
