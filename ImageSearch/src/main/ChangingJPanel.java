e041e5e6efb5796d0ad74dd37760a74f9d082bf3
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-14 00:51:40 +0900

    [ADD] 검색어 기록을 표시할 3번째 패널 생성

--- ImageSearch/src/main/ChangingJPanel.java
+++ ImageSearch/src/main/ChangingJPanel.java
@@ -8,12 +8,20 @@ public class ChangingJPanel extends JFrame{
 	
 	public JPanel01 jpanel01 = null; 
 	public JPanel02 jpanel02 = null; 
+	public JPanel03 jpanel03 = null; 
 	
 	public void ChangePanel(String panelName) {
 		
 		if(panelName.equals("panel01")) {
 			getContentPane().removeAll();
 			getContentPane().add(jpanel01);
+			revalidate();
+			repaint();
+		}
+		
+		else if (panelName.equals("panel03")) {
+			getContentPane().removeAll();
+			getContentPane().add(jpanel03);
 			revalidate();
 			repaint();
 		}
@@ -26,4 +34,11 @@ public class ChangingJPanel extends JFrame{
 		}
 	}
 	
+//	public RunChange(class panel) {
+//		getContentPane().removeAll();
+//		getContentPane().add(panel);
+//		revalidate();
+//		repaint();
+//	}
+//	
 }
