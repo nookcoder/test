acd55fdc98dc85d1ed490c385c8b6bce1cda85c4
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-14 02:19:17 +0900

    [SET] 이미지 url ArrayList로 반환하도록 변경

--- ImageSearch/src/main/KakaoCrawler.java
+++ ImageSearch/src/main/KakaoCrawler.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -26,6 +27,7 @@ import org.json.simple.parser.ParseException;
 public class KakaoCrawler {
 
 	 public enum HttpMethodType{POST,GET,DELETE}
+	 public ArrayList<String> urlList;
 	 
 	 private static final String API_SERVER_HOST = "https://dapi.kakao.com";
 	 private static final String IMAGE_PATH = "/v2/search/image.json?query=";
@@ -33,9 +35,11 @@ public class KakaoCrawler {
 	 // 카카오에서 한줄 정보(?) 받아오기 
 	 public String GetJsonFromKakao(String searchText) throws IOException
 	 {
+		 this.urlList = new ArrayList<String>();
+		 
 		 String str = null;
 		 try {
-	        	String query = URLEncoder.encode(searchText,"UTF-8 ");
+	        	String query = URLEncoder.encode(searchText,"UTF-8");
 	            String apiURL = API_SERVER_HOST + IMAGE_PATH + query;
 	            URL url = new URL(apiURL);
 	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
@@ -73,22 +77,20 @@ public class KakaoCrawler {
 	 }
 	
 	 // 이미지 Url 로 이미지 넣기 
-	 public void AddImage(Container c,String SearchText) throws ParseException, IOException
+	 public ArrayList<String> AddImage(String SearchText) throws ParseException, IOException
 	 {
 		 JSONArray imgUrlArray = GetImageUrlArray(SearchText);
-		 URL url;
-		 JLabel label;
-		
+
 		 for(int i =0;i<imgUrlArray.size() ;i++)
 		 {
 			 JSONObject jsonobj = (JSONObject)imgUrlArray.get(i);
-			 String imgUrl = (String)jsonobj.get("image_url");
-			 url = new URL(imgUrl);
-			 BufferedImage image = ImageIO.read(url);
-			 label = new JLabel(new ImageIcon(image));
-			 c.add(label);
-		 }
+			 urlList.add((String)jsonobj.get("image_url"));
+		}
+		 
+		 return urlList;
+	}
+
 
 	}
 	
-}
+
