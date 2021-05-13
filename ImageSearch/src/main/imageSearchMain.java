6eb60c4f7e603264aa83889d828a3dd1a9180ff5
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-13 17:56:17 +0900

    [ADD] 자바 프레임 실행문? 추가

--- ImageSearch/src/main/imageSearchMain.java
+++ ImageSearch/src/main/imageSearchMain.java
@@ -1,19 +1,27 @@
 package main;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+
+import javax.swing.JFrame;
+
+import org.json.simple.parser.ParseException;
 
 public class ImageSearchMain {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParseException{
 		// TODO Auto-generated method stub
-		new Frame();
-		KakaoCrawler kakao = new KakaoCrawler();
-		kakao.Get();
+//		new Frame();
+
+		ChangingJPanel win = new ChangingJPanel();
+		
+		win.setTitle("이미지 검색");
+		win.jpanel01 = new JPanel01(win);
+		win.jpanel02 = new JPanel02(win);
+		
+		win.add(win.jpanel01);
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		win.setSize(800,600);
+		win.setVisible(true);
 	}
 
 }
