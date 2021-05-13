6d4cd865987f3528ab45aa21c2791cf2ed42c704
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-13 16:02:39 +0900

    [ADD] 패널 1,2 class 생성

---
+++ ImageSearch/src/main/JPanel01.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JPanel01 extends JPanel{ // 1번째 패널 
+
+	private JButton jButton1;
+	private JScrollPane jscrolPane1;
+	private JTextArea jTextArea1; 
+}
---
+++ ImageSearch/src/main/JPanel02.java
@@ -0,0 +1,8 @@
+package main;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JPanel02 extends JPanel{
+
+}
