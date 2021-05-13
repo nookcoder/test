226c812fdde857f08f9c30aadadd074c1f812c5c
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-13 17:56:32 +0900

    [ADD] 첫번째 화면 클래스 추가

--- ImageSearch/src/main/JPanel01.java
+++ ImageSearch/src/main/JPanel01.java
@@ -2,10 +2,56 @@ package main;
 
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class JPanel01 extends JPanel{ // 1번째 패널 
 
 	private JButton jButton1;
 	private JScrollPane jscrolPane1;
-	private JTextArea jTextArea1; 
+	private JTextField jTextField;
+	private ChangingJPanel change;
+	
+	public JPanel01(ChangingJPanel change) {
+		this.change = change;
+		setLayout(null);
+	
+		jButton1 = new JButton("검색");
+		jButton1.setSize(100,60);
+		jButton1.setLocation(600,250);
+		
+		jButton1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				change.ChangePanel("panel02");
+			}
+		});
+		
+		add(jButton1);
+		
+		jTextField = new JTextField("") {
+			@Override
+			public void setBorder(Border border)
+			{
+				
+			}
+		};
+		
+		jTextField.setSize(500,60);
+		jTextField.setLocation(100,250);
+		
+		add(jTextField); 
+		
+		jTextField.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				jTextField.setText("");
+			}
+		});
+		
+		
+	}
+	
+	
 }
