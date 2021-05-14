c227ab93747922ffa88f9ee111ac5f406fd51616
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-14 13:49:08 +0900

    [ADD] mysql Insert/delete/select 기능 추가

--- ImageSearch/src/main/SearchLog.java
+++ ImageSearch/src/main/SearchLog.java
@@ -1,33 +1,59 @@
 package main;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
-public class SearchLog {
 
-	public void SearchLogDBConnection() {
-		Connection conn = null;
+public class SearchLog { // mysql 에서 활동내역을 관리할 클래스 
+	
+	private Constants constant;
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private SimpleDateFormat dateFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+	
+	public SearchLog() {
 		
+		this.constant = new Constants();
+		
+		// mysql 에 연결하기 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			String url = "jdbc:mysql://localhost/root";
-			conn = DriverManager.getConnection(url,"root","0000");
-			System.out.println("연결 성공");
+			Class.forName(constant.JDBC_DRIVER);
+			conn = DriverManager.getConnection(constant.DB_URL,constant.USERNAME,constant.PASSWORD);
+			
+		} catch (ClassNotFoundException e) { 
+			e.printStackTrace(); 
+		} catch (SQLException e) { 
+			e.printStackTrace();
 		}
-		 catch(ClassNotFoundException e){
-	            System.out.println("드라이버 로딩 실패");
-	        }
-	        catch(SQLException e){
-	            System.out.println("에러: " + e);
-	        }
-	        finally{
-	            try{
-	                if( conn != null && !conn.isClosed()){
-	                    conn.close();
-	                }
-	            }
-	            catch( SQLException e){
-	                e.printStackTrace();
-	            }
-	        }
+	}
+	
+	// 활동 내역 DB 에 저장하기 
+	public void InsertSearchLog(String text) throws SQLException {
+		String InsertQuery = constant.INSERTQUERY; 
+		pstmt = conn.prepareStatement(InsertQuery);
+		pstmt.setString(1,text);
+		pstmt.setString(2,dateFormat.format(System.currentTimeMillis()).toString());
+		pstmt.executeUpdate();
+	}
+	
+	// 활동 내역 초기화 
+	public void DeleteAll() throws SQLException {
+		String deleteQuery = constant.DELETEQUERY;
+		pstmt = conn.prepareStatement(deleteQuery);
+		pstmt.executeUpdate();
+	}
+	
+	// 활동 내역 출력하기 
+	public void ShowSearchLog() throws SQLException {
+		String selectQuery = constant.SELECTQUERY;
+		String searchLogStr ="";
+		pstmt = conn.prepareStatement(selectQuery);
+		ResultSet rs = pstmt.executeQuery(selectQuery);
+		
+		while(rs.next()) {
+			searchLogStr += "검색 시간 : "+rs.getString("date") + "\t검색어 : "+rs.getString("text") +"\n";
+		}
+		
 	}
 }
