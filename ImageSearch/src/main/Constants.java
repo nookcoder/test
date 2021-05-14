ae0f619b295ff61140806888aa7ba0dc3f2ffbc5
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-14 13:50:15 +0900

    [ADD] MySql DB 관련 문자열 Constant 처리

---
+++ ImageSearch/src/main/Constants.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Constants {
+
+	// mysql ฐüทร 
+	public String JDBC_DRIVER = "com.mysql.jdbc.Driver"; 
+	public String DB_URL = "jdbc:mysql://localhost:3306/search_log?serverTimezone=Asia/Seoul&useSSL=false";
+	public String USERNAME = "root";
+	public String PASSWORD = "0000";
+	public String INSERTQUERY = "insert into searchrecord(text,date) values(?,?)";
+	public String DELETEQUERY = "delete from searchrecord";
+	public String SELECTQUERY = "select * from searchredcord";
+}
