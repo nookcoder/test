af965f983cca75566a9c053349ebcad71fba39fe
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-17 21:35:04 +0900

    [ADD] 덧셈기능 추가중

--- Calculator_김현욱/src/main/SettingCalculatorDisplay.java
+++ Calculator_김현욱/src/main/SettingCalculatorDisplay.java
@@ -34,9 +34,19 @@ public class SettingCalculatorDisplay extends JPanel
 	
 	private EventListener eventListener;
 	
+	public String lastedOperator;
+	private int num1, num2;
+	private int sum;
+	public boolean isStart = true; 
+	public boolean isContinue = false;
+	public boolean isEqualNext = false;
+	
 	public SettingCalculatorDisplay() {
 		this.constant = new Constants();
 		this.eventListener = new EventListener();
+		this.num1 = 0; 
+		this.num2 = 0; 
+		this.sum = 0;
 		
 		this.displayPanel = new JPanel(); 
 		this.keyPadPanel = new JPanel();
@@ -49,7 +59,7 @@ public class SettingCalculatorDisplay extends JPanel
 		textArea.setHorizontalAlignment(JTextField.RIGHT);
 		textArea.setFont(constant.font);
 		
-		add(textArea);
+		add(textArea,BorderLayout.NORTH);
 		
 		this.keyPadPanel = new JPanel(); 
 		this.numberButton = new JButton[10];
@@ -60,31 +70,93 @@ public class SettingCalculatorDisplay extends JPanel
 			setButtonFont(numberButton[number]);
 			
 			numberButton[number].addActionListener(new ActionListener() {
+			
+				// 계산기 버튼 클릭 시 입력되게 하는 함수 
 				public void actionPerformed(ActionEvent e) {
 					JButton numberButton = (JButton)e.getSource();
 					String number = numberButton.getText();
 					String result = textArea.getText(); 
-					String text = "";
-					if(result.equals("0"))
+					if(isStart||result.equals("0"))
 					{
 						textArea.setText("");
 						textArea.setText(number);
-					}					
+					}			
+					
+					else if(isContinue)
+					{
+						textArea.setText("");
+					}
+					
 					else {textArea.setText(result + number);}
+					
+					isStart = false;
 				}
 			});
 		}
 		
 		this.plus = new JButton("+");
+		plus.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e ) {
+				
+				JButton operatorButton = (JButton)e.getSource();
+				String operator = operatorButton.getText();
+				if(operator == "+")
+				{
+					if(isEqualNext)
+					{
+						textArea.setText("");
+					}
+					
+					else
+					{
+						num1 = Integer.parseInt(textArea.getText());
+						num2 = Integer.parseInt(textArea.getText());
+						sum += num1;
+						num1 = 0;
+						lastedOperator = "+";
+						isStart = true;
+					}
+					
+				}
+						
+			}
+			
+		});
 		this.minus = new JButton("ㅡ");
 		this.multiply = new JButton("X");
 		this.divide = new JButton("%");
 		this.c = new JButton("C");
 		this.ce = new JButton("CE");
 		this.dot = new JButton(".");
-		this.changingSign = new JButton("+/-"
-				+ "");
+		this.changingSign = new JButton("+/-");
 		this.equal = new JButton("=");
+		equal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				JButton operatorButton = (JButton)e.getSource();
+				String operator = operatorButton.getText();
+				if(operator == "=") {
+					if(lastedOperator == "+")
+					{
+						if(isEqualNext)
+						{
+							num2 = Integer.parseInt(textArea.getText());
+							sum += num2;
+							isEqualNext = true;
+						}
+						
+						else 
+						{
+							sum += num2;
+							isEqualNext = true;
+						}
+					}
+					textArea.setText(Integer.toString(sum));
+				}
+			}
+		});
+		
 		this.backSpace = new JButton("BACK");
 		
 		setSignButtonFont(plus);
@@ -96,9 +168,7 @@ public class SettingCalculatorDisplay extends JPanel
 		setSignButtonFont(changingSign);
 		setSignButtonFont(equal);
 		setSignButtonFont(backSpace);
-		
-		
-		plus.addActionListener(null);
+
 		minus.addActionListener(null);
 		multiply.addActionListener(null);
 		divide.addActionListener(null);
@@ -136,7 +206,7 @@ public class SettingCalculatorDisplay extends JPanel
 		
 		keyPadPanel.setLayout(new GridLayout(5,4));
 		
-		add(keyPadPanel);
+		add(keyPadPanel,BorderLayout.CENTER);
 		
 	}
 	
