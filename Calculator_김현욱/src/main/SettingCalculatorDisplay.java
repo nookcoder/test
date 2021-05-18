026ffbbf67f38912887565878186979f0950c2b7
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-18 13:59:57 +0900

    [SET] 주석 추가

--- Calculator_김현욱/src/main/SettingCalculatorDisplay.java
+++ Calculator_김현욱/src/main/SettingCalculatorDisplay.java
@@ -32,21 +32,26 @@ public class SettingCalculatorDisplay extends JPanel
 	
 	private Constants constant;
 	
-	public String lastedOperator;
-	private int num;
-	private int sum;
-	private boolean isNewNumberStart  = true; 
-	private boolean isContinue = false;
-	private boolean isEqualNext = false;
-	private boolean isFirstNumber = true;
+	public String lastedOperator; // 가장 최근에 입력된 사칙연산 기호 
+	private int num; // 입력된 숫자 
+	private int sum; // 출력될 계산 결과 값
+	private boolean isNewNumberStart  = true; // 새로운 숫자가 입력되는 지 판단
+	private boolean isEqualNext = false; // = 를 누른 후 다음 이벤트인지 판단
+	private boolean isFirstNumber = true; // 계산 과정에서 첫번째 입력 숫자인지 확인 
 	
 	public SettingCalculatorDisplay() {
 		this.constant = new Constants();
+		
+		// 계산과정에서 쓰일 변수들 초기화 
 		this.num = 0; 
 		this.sum = 0;
+		this.lastedOperator = null;
+		this.isNewNumberStart = true;
+		this.isFirstNumber = true;
+		this.isEqualNext = false;
 		
-		this.displayPanel = new JPanel(); 
-		this.keyPadPanel = new JPanel();
+		this.displayPanel = new JPanel(); // 숫자표시칸 관련 패널 
+		this.keyPadPanel = new JPanel(); // 숫자패드 관련 패널 
 		
 		displayPanel.setLayout(new FlowLayout());
 		keyPadPanel.setLayout(new GridLayout(0,4,5,5));
@@ -61,6 +66,7 @@ public class SettingCalculatorDisplay extends JPanel
 		this.keyPadPanel = new JPanel(); 
 		this.numberButton = new JButton[10];
 		
+		// 숫자 버튼 설정 
 		for(int number = 0; number<10;number++)
 		{
 			numberButton[number] = new JButton(Integer.toString(number));
@@ -143,12 +149,15 @@ public class SettingCalculatorDisplay extends JPanel
 		btn.setFont(constant.signFont);
 	}
 
+	// 숫자 키 입력 시 이벤트 처리 
 	private class NumberButtonListener implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e){
 			JButton numberButton = (JButton)e.getSource();
 			String number = numberButton.getText();
 			String result = textArea.getText(); 
+			
+			// 새로운 숫자 입력시 기존 쓰여져있던 숫자 지우기 
 			if(isNewNumberStart ||result.equals("0")||isEqualNext)
 			{
 				textArea.setText("");
@@ -156,15 +165,33 @@ public class SettingCalculatorDisplay extends JPanel
 			}			
 			
 			
+			// 연속으로 숫자 입력 시 
 			else {textArea.setText(result + number);}
 			
+			// 입력한 숫자 저장 
 			num = Integer.parseInt(textArea.getText());
+			
+			// 연산 기호 다음 숫자일 떄 처리(계산해주는 과정) 
+			if(lastedOperator != null)
+			{
+				RunOperator(lastedOperator);
+			}
 			
 			isNewNumberStart = false;
 			isEqualNext = false;
 		}
+		
+		// 계산하는 함수 
+		public void RunOperator(String lastedOperator)
+		{
+			if(lastedOperator == "+") {sum += num;}
+			else if(lastedOperator == "ㅡ") {sum -= num;}
+			else if(lastedOperator == "X") {sum *= num;}
+			else if(lastedOperator == "/") {sum /= num;}
+		}
 	}
 	
+	// 연산 기호 이벤트 처리 
 	private class OperatorListener implements ActionListener{
 		public void actionPerformed(ActionEvent e ) {
 			
@@ -183,24 +210,23 @@ public class SettingCalculatorDisplay extends JPanel
 					isFirstNumber = false;
 				}
 				
-				else 
-				{
-					if(operator == "+") {sum += num;}
-					else if(operator == "ㅡ") {sum -= num;}
-					else if(operator == "X") {sum *= num;}
-					else if(operator == "/") {sum /= num;}
-				}
-				
 				lastedOperator = operator;
 				isNewNumberStart  = true;
 		}
 		
 	}
 	
+	// 등호(=) 이벤트 처리 
 	private class EqualListener implements ActionListener{
 		public void actionPerformed(ActionEvent e)
 		{
-			RunOperation(lastedOperator);
+			// 등호를 연속으로 입력했을 떄 
+			if(isEqualNext)
+			{
+				RunOperation(lastedOperator);
+			}
+			
+			isEqualNext = true;
 			textArea.setText(Integer.toString(sum));
 		}
 		
@@ -210,36 +236,38 @@ public class SettingCalculatorDisplay extends JPanel
 			else if(operator == "ㅡ") {sum -= num;}
 			else if(operator == "X") {sum *= num;}
 			else if(operator == "/") {sum /= num;}
-			isEqualNext = true;
 		}
 		
 	}
 	
+	// c ce 입력시 이벤트 처리 
 	private class ResetListener implements ActionListener{
 		
 		public void actionPerformed(ActionEvent e)
 		{
 			JButton resetButton = (JButton)e.getSource();
+			
+			// 완전 초기화 
 			if(resetButton.getText() == "C")
 			{
 				num = 0; 
 				isNewNumberStart  = true; 
-				isContinue = false;
 				isEqualNext = false;
 				isFirstNumber = true;
 				sum = 0;
 				textArea.setText("0");
 			}
 			
+			// 등호 다음 ce 입력 시 초기화, 계산과정중 ce입력시 숫자만 초기화 
 			else if (resetButton.getText() == "CE")
 			{
 				textArea.setText("0");
 				if(isEqualNext) {
-					num = 0; 
 					isNewNumberStart  = true; 
-					isContinue = false;
 					isEqualNext = false;
 					isFirstNumber = true;
+					lastedOperator = null;
+					num = 0; 
 					sum = 0;
 				}
 			}
