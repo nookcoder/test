bb75ed6c8c79dfda0ec249202da006e3f2caf941
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-19 13:58:52 +0900

    [ADD] 계산기에서 쓸 회색 추가

--- Calculator_김현욱/src/main/Constants.java
+++ Calculator_김현욱/src/main/Constants.java
@@ -1,9 +1,12 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Font;
 
 public class Constants {
 
-	Font font = new Font("µ¸À½", Font.BOLD, 30);
+	Font displayFont = new Font("µ¸À½",Font.BOLD,30);
+	Font font = new Font("µ¸À½", Font.BOLD, 20);
 	Font signFont = new Font("µ¸À½", Font.BOLD, 15);
+	Color calcGray = new Color(242,242,242);
 }
