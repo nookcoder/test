2bccc1ff5a0f01a524d539f3a815b64facb4c4a2
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-22 12:08:42 +0900

    [ADD] 입력창 배경색 변경, border 삭제

---
+++ Calculator2_김현욱/Calculator2_김현욱/src/set/Constants.java
@@ -0,0 +1,18 @@
+package set;
+import java.awt.*;
+
+import javax.swing.JButton;
+
+public class Constants {
+
+	public GridLayout textGridLayout = new GridLayout(0,1); 
+	public GridLayout panelGridLayout = new GridLayout(0,2); 
+	public BorderLayout KeyPadBorderLayout = new BorderLayout();
+	public Font ButtonFont = new Font("µ¸À½", Font.BOLD, 20);
+	
+	public void setButtonFont(JButton btn) {
+		btn.setFont(ButtonFont);
+		btn.setBorderPainted(false);
+		btn.setBackground(Color.WHITE);
+	}
+}
