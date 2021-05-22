f76d8f091077f1f9062d0e4915fa2ac4578fa309
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-22 12:08:11 +0900

    no message

---
+++ Calculator2_김현욱/Calculator2_김현욱/src/main.java
@@ -0,0 +1,10 @@
+import set.SettingCalculator;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SettingCalculator settingCalulator = new SettingCalculator();
+	}
+
+}
