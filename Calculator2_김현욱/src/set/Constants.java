606a39063aaa1e255f8f1dd0e0e08c2d468666e4
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-24 23:39:10 +0900

    [ADD] 계산기 출력 글자 폰트 추가

--- Calculator2_김현욱/src/set/Constants.java
+++ Calculator2_김현욱/src/set/Constants.java
@@ -9,6 +9,13 @@ public class Constants {
 	public GridLayout panelGridLayout = new GridLayout(0,2); 
 	public BorderLayout KeyPadBorderLayout = new BorderLayout();
 	public Font ButtonFont = new Font("µ¸À½", Font.BOLD, 20);
+	public Font calculatorDisplayFont = new Font("µ¸À½",Font.BOLD,50);
+	public Font calculatorDisplayFont2 = new Font("µ¸À½",Font.BOLD,45);
+	public Font calculatorDisplayFont3 = new Font("µ¸À½",Font.BOLD,40);
+	public Font calculatorDisplayFont4 = new Font("µ¸À½",Font.BOLD,38);
+	public Font calculatorDisplayFont5 = new Font("µ¸À½",Font.BOLD,36);
+	public Font calculatorDisplayFont6 = new Font("µ¸À½",Font.BOLD,34);
+	public Font calculatorDisplayFont7 = new Font("µ¸À½",Font.BOLD,32);
 	
 	public void setButtonFont(JButton btn) {
 		btn.setFont(ButtonFont);
