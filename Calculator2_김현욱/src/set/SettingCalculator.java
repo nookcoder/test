823f08b98b53e2867460e15ae6b8ecda977c669b
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-27 19:01:00 +0900

    [ADD] 커지면 옆에 기록창 생기는 기능 추가!!!

--- Calculator2_김현욱/src/set/SettingCalculator.java
+++ Calculator2_김현욱/src/set/SettingCalculator.java
@@ -1,5 +1,8 @@
 package set;
 
+import java.awt.BorderLayout;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -13,44 +16,67 @@ import text.TextPanel;
 public class SettingCalculator extends JFrame{
 	
 	private Constants constats = new Constants(); 
+	private JPanel contentPanel = new JPanel();
+	public TextPanel textPanel; 
+	public BigTextPanel bigTextPanel;
 	
 	public SettingCalculator() {
-		
+		this.textPanel = new TextPanel();
+		this.bigTextPanel = new BigTextPanel();
+	
 		setTitle("°è»ê±â");
 		setSize(350,500);
 		setMinimumSize(constats.dimension);
+		
 		if(getWidth() < 350 && getHeight() < 500)
 		{
 			setSize(350,500);
 		}		
-		TextPanel textPanel = new TextPanel();
-		BigTextPanel bigTextPanel = new BigTextPanel();
-		add(textPanel);
-		this.addMouseMotionListener(new MouseMotionListener() {
-
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-				int width = getWidth();
-				if(textPanel.get >= 600)
-				{
-					removeAll();
-					add(bigTextPanel);
-					revalidate();
-					repaint();
-					
-				}
-			}
-
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-		});
+		
+		contentPanel.setLayout(new BorderLayout());
+		contentPanel.add(textPanel,BorderLayout.CENTER);
+		contentPanel.addComponentListener(new SizeListener());
 		//add(bigTextPanel);
+		add(contentPanel);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+	}
+	
+	private class SizeListener implements ComponentListener{
+		@Override
+		public void componentResized(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getComponent().getWidth() > 900)
+			{
+				contentPanel.removeAll();
+				contentPanel.add(bigTextPanel,BorderLayout.CENTER);
+				contentPanel.updateUI();
+				contentPanel.repaint();
+			}
+			else if(e.getComponent().getWidth() <= 900) {
+				contentPanel.removeAll();
+				contentPanel.add(textPanel,BorderLayout.CENTER);
+				contentPanel.updateUI();
+				contentPanel.repaint();
+			}
+		}
+
+		@Override
+		public void componentMoved(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void componentShown(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void componentHidden(ComponentEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 }
