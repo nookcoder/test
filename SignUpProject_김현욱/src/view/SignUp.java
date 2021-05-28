22f832b9b75d81d3f587548729e968fddb922a8b
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-28 21:42:09 +0900

    [SET] textField 둥근 테두리로 변경

--- SignUpProject_김현욱/src/view/SignUp.java
+++ SignUpProject_김현욱/src/view/SignUp.java
@@ -6,13 +6,18 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.AbstractBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import java.awt.*;
 import javax.swing.JLabel;
 import javax.swing.SpringLayout;
 import java.awt.Font;
+import java.awt.geom.Area;
+import java.awt.geom.*;
+
 import javax.swing.SwingConstants;
+import javax.swing.*;
 
 public class SignUp extends JFrame {
 
@@ -48,6 +53,8 @@ public class SignUp extends JFrame {
 	public SignUp() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(400,600);
+		setResizable(false);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -101,46 +108,217 @@ public class SignUp extends JFrame {
 		addressLabel.setBounds(43, 434, 59, 53);
 		contentPane.add(addressLabel);
 		
-		idField = new JTextField();
+		idField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
+		idField.setBorder(null);
 		idField .setBounds(114, 18, 225, 36);
-		contentPane.add(idField);
 		idField.setColumns(10);
+		contentPane.add(idField);
 		
-		passwordField = new JTextField();
+		passwordField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		passwordField.setColumns(10);
 		passwordField.setBounds(114, 79, 225, 36);
 		contentPane.add(passwordField);
 		
-		passwordCheckField = new JTextField();
+		passwordCheckField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		passwordCheckField.setColumns(10);
 		passwordCheckField.setBounds(114, 132, 225, 36);
 		contentPane.add(passwordCheckField);
 		
-		nameField = new JTextField();
+		nameField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		nameField.setColumns(10);
 		nameField.setBounds(114, 193, 225, 36);
 		contentPane.add(nameField);
 		
-		birthField = new JTextField();
+		birthField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		birthField.setColumns(10);
 		birthField.setBounds(114, 253, 225, 36);
 		contentPane.add(birthField);
 		
-		phoneNumberField = new JTextField();
+		phoneNumberField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		phoneNumberField.setColumns(10);
 		phoneNumberField.setBounds(114, 320, 225, 36);
 		contentPane.add(phoneNumberField);
 		
-		emailField = new JTextField();
+		emailField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		emailField.setColumns(10);
 		emailField.setBounds(114, 381, 225, 36);
 		contentPane.add(emailField);
 		
-		addressField = new JTextField();
+		addressField = new JTextField() {
+			@Override 
+			protected void paintComponent(Graphics g) {
+				if (!isOpaque() && getBorder() instanceof RoundedCornerBorder) {
+					Graphics2D g2 = (Graphics2D) g.create();
+					g2.setPaint(getBackground());
+					g2.fill(((RoundedCornerBorder) getBorder()).getBorderShape(
+							0, 0, getWidth() - 1, getHeight() - 1));
+					g2.dispose();
+				}
+				super.paintComponent(g);
+			}
+			@Override 
+			public void updateUI() {
+				super.updateUI();
+				setOpaque(false);
+				setBorder(new RoundedCornerBorder());
+			}
+		};
 		addressField.setColumns(10);
 		addressField.setBounds(114, 443, 225, 36);
 		contentPane.add(addressField);
-		
-
+	}
+	
+	public class RoundedCornerBorder extends AbstractBorder {
+		private Color ALPHA_ZERO = new Color(0x0, true);
+		@Override 
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+			Graphics2D g2 = (Graphics2D) g.create();
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			Shape border = getBorderShape(x, y, width - 1, height - 1);
+			g2.setPaint(ALPHA_ZERO);
+			Area corner = new Area(new Rectangle2D.Double(x, y, width, height));
+			corner.subtract(new Area(border));
+			g2.fill(corner);
+			g2.setPaint(Color.GRAY);
+			g2.draw(border);
+			g2.dispose();
+		}
+		public Shape getBorderShape(int x, int y, int w, int h) {
+			int r = h; //h / 2;
+			return new RoundRectangle2D.Double(x, y, w, h, r, r);
+		}
+		@Override public Insets getBorderInsets(Component c) {
+			return new Insets(4, 8, 4, 8);
+		}
+		@Override public Insets getBorderInsets(Component c, Insets insets) {
+			insets.set(4, 8, 4, 8);
+			return insets;
+		}
 	}
 }
