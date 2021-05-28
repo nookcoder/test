b2c3a0581f94f08510cfb3fea23d4e565eb769a5
Hyeon Wook Kim <gus70005319@gmail.com>
2021-05-29 04:48:03 +0900

    [ADD] 데베에 정보를 넣는 함수 추가

---
+++ SignUpProject_김현욱/src/Controller/SignUpController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import java.awt.event.*;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.*;
+
+import model.Constants;
+import model.MemberDataBase;
+import view.SignUp;
+
+public class SignUpController {
+
+	private MemberDataBase member;
+	private SignUp signup;
+	private Constants constants;
+	
+	public SignUpController(MemberDataBase member, SignUp signup)
+	{
+		this.constants = new Constants();
+		this.member = member;
+		this.signup = signup;
+	}
+	
+	// 데베에 정보 넣기 
+	public void insertData(String id,String password,String name,String birth,String email,String phoneNumber,String address) throws SQLException
+	{
+		String insertQuery = constants.INSERTQUERY;
+		PreparedStatement statement = (PreparedStatement) member.connection.prepareStatement(insertQuery);
+		statement.setString(1, id);
+		statement.setString(2, password);
+		statement.setString(3, name);
+		statement.setString(4, birth);
+		statement.setString(5, email);
+		statement.setString(6, phoneNumber);
+		statement.setString(7, address);
+		statement.executeUpdate();
+	}
+	
+	public void checkIsHaving()
+	{
+		String selecQuery = constants.SELECTQUERY;
+	}
+	
+//	private class InsertListener implements ActionListener{
+//		
+//	}
+}
