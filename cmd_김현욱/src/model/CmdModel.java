06ab8435f1dd9a87117ed2a516e67ecab3e10457
Hyeon Wook Kim <gus70005319@gmail.com>
2021-06-04 20:09:20 +0900

    [ADD] model copy로직추가

--- cmd_김현욱/src/model/CmdModel.java
+++ cmd_김현욱/src/model/CmdModel.java
@@ -1,6 +1,9 @@
 package model;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CmdModel {
@@ -39,5 +42,30 @@ public class CmdModel {
 		routeName = routeName.substring(0, seperateIndex);
 
 		return routeName;
+	}
+	
+	public void copyFile(String originalPath, String copyPath) throws FileNotFoundException
+	{
+		int fileByte;
+		
+		File originalFile = new File(originalPath);
+		File copyFile = new File(copyPath);
+		
+		FileInputStream InputFile = new FileInputStream(originalFile); 
+		FileOutputStream outFile = new FileOutputStream(copyFile);
+	
+		fileByte = 0;
+		try {
+			while((fileByte = InputFile.read()) != -1)
+			{
+				outFile.write(fileByte);
+			}
+			
+			InputFile.close();
+			outFile.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
