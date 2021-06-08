8e9705a787007c7fce1171059c93e74980ea3a36
Hyeon Wook Kim <gus70005319@gmail.com>
2021-06-08 13:32:14 +0900

    [FIX] move 파일에서 파일로 이동 오류 수정

--- cmd_김현욱/src/controller/MoveStatement.java
+++ cmd_김현욱/src/controller/MoveStatement.java
@@ -1,23 +1,31 @@
 package controller;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import model.CmdModel;
+import model.Constants;
+import model.Copy;
 import view.CmdView;
 
 public class MoveStatement {
 	private CmdView view;
 	private CmdController controller;
 	private CmdModel model;	
+	private Copy copy; 
+	private Scanner scan; 
+	private Constants constants;
 	
 	public MoveStatement(CmdView view,CmdController controller,CmdModel model)
 	{
 		this.view = view;
 		this.controller = controller;
 		this.model = model;
+		this.copy = new Copy();
+		this.scan = new Scanner(System.in);
+		this.constants = new Constants();
 	}
 	
 	// copy 명령문 실행 
@@ -25,25 +33,7 @@ public class MoveStatement {
 	{
 		if(userStatementList.size() == 3)
 		{
-			// 같은경로에 복사할때 
-			if(!userStatementList.get(1).contains(File.separator) && !userStatementList.get(2).contains(File.separator))
-			{
-				moveFileWithNoPath(userStatementList);
-			}
-			else if(userStatementList.get(1).contains(File.separator) && !userStatementList.get(2).contains(File.separator))
-			{
-				moveFileWithNoPath(userStatementList);
-			}
-			
-			else if(userStatementList.get(1).contains(File.separator) && !userStatementList.get(2).contains(File.separator))
-			{
-				moveFileWithNoPath(userStatementList);
-			}
-			// 경로가 모두 입력될때 
-			else if(userStatementList.get(1).contains(File.separator) && userStatementList.get(2).contains(File.separator))
-			{
-				moveFileWithNoPath(userStatementList);
-			}
+			runMove(userStatementList);
 		}
 		
 		else if(userStatementList.size() == 1)
@@ -53,68 +43,85 @@ public class MoveStatement {
 		}
 	}
 	
-	
-	public void runMove(String originalPath, String movePath) throws IOException
+	public void runMove(List<String> userStatementList) throws IOException
 	{
-		// 복사할 파일이 존재할 때 
-				if(model.isExistsFile(originalPath))
-				{
-					File originalFile = new File(originalPath);
-					File copyFile = new File(movePath);
-					if(originalFile.isDirectory())
-					{
-						model.copyDirectory(originalFile,copyFile);	
-						model.deleteFile(originalPath);
-					}
-					
-					else
-					{
-						model.copyFile(originalFile, copyFile);
-						model.deleteFile(originalPath);
-					}
-					
-					view.showSuccessMove();
-					view.showRoute(controller.routeName);
-				}
+		String sourcePath = model.makeCopyPathName(controller.routeName,userStatementList.get(1));
+		String movePath = model.makeCopyPathName(controller.routeName,userStatementList.get(2));
+
+		
+		File source = new File(sourcePath); 
+		File destination = new File(movePath);
+		
+		int catagory = model.checkCatagory(source, destination);
+		
+		switch(catagory)
+		{
+			//case constants.FILE_FILE:
+			case 0:
+				view.showNoExistsOriginalFile();
+				view.showRoute(controller.routeName);
+				break;
+			case 1: // File_File
+				moveFileToFile(sourcePath,movePath,userStatementList);
+				break;
 				
-				//isDirectory 추가 
+			case 2: // File_Directory
+				break;
 				
-				// 복사파일이 없을 때 
-				else
-				{
-					view.showNoExistsOriginalFile();
-					view.showRoute(controller.routeName);
-				}
+			case 3: // Directory_File
+				view.showFailExcess();
+				view.showSuccessMove(0);
+				view.showRoute(controller.routeName);
+				break;
+				
+			case 4: // Directory_Directory
+				break;
+		}
 	}
 	
-	public void moveFileWithNoPath(List<String> userStatementList) throws IOException
+	public void moveFileToFile(String sourcePath, String copyPath,List<String> userStatementList) throws IOException
 	{
-		String originalPath = controller.routeName + File.separator+userStatementList.get(1);
-		String copyPath = controller.routeName + File.separator+userStatementList.get(2);
-		runMove(originalPath,copyPath);
+		File source = new File(sourcePath); 
+		File destination = new File(copyPath);
+		
+		if(sourcePath.equals(copyPath))
+		{
+			view.showSuccessMove(1);
+			view.showRoute(controller.routeName);
+			return;
+		}
+		
+		if(destination.exists())
+		{
+			String answer = constants.INITIALIZATION_STRING;
+			while(answer.charAt(0) != 'y' && answer.charAt(0) != 'a' && answer.charAt(0) != 'n')
+			{
+				view.askOverWrite(userStatementList.get(2));
+				answer = scan.next().toLowerCase();
+				
+			}
+			if(answer.charAt(0) == 'y' || answer.charAt(0) == 'a')
+			{
+				copy.FileToFile(source, destination);
+				destination.delete();
+				view.showSuccessMove(1);
+				view.showRoute(controller.routeName);
+				return;
+			}
+			
+			else if(answer.charAt(0) == 'n')
+			{
+				view.showSuccessMove(1);
+				view.showRoute(controller.routeName);
+				return;
+			}
+		
+		}
+		
+		copy.FileToFile(source, destination);
+		destination.delete();
+		view.showSuccessCopy();
+		view.showRoute(controller.routeName);
 	}
-	
-	// 이동해올 파일 경로만 일력됐을 때 
-	public void moveFileWithOriginalPath(List<String> userStatementList) throws IOException
-	{
-		String originalPath = userStatementList.get(1);
-		String copyPath = controller.routeName + File.separator+userStatementList.get(2);
-		runMove(originalPath,copyPath);
-	}
-	
-	// 이동될 파일 경로만 입력됐을 때 
-	public void moveFileWithCopyPath(List<String> userStatementList) throws IOException
-	{
-		String originalPath = controller.routeName + File.separator+userStatementList.get(1);
-		String copyPath = userStatementList.get(2);
-		runMove(originalPath,copyPath);
-	}
-	
-	// 경로 두개 입력 됐을 때 
-	public void moveFileWithTwoPath(List<String> userStatementList) throws IOException
-	{
-		String originalPath = userStatementList.get(1);
-		String copyPath = userStatementList.get(2);
-		runMove(originalPath,copyPath);
-	}
+
 }
